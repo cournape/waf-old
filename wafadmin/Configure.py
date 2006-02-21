5faf5510340dbbea5fffdcf07fa7796bfb71f180
habacker <habacker@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2006-02-21 11:15:51 +0000

    configure class now supports some more functions
    git-svn-id: http://waf.googlecode.com/svn/trunk@31 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Configure.py
+++ wafadmin/Configure.py
@@ -46,14 +46,18 @@ def find_program_using_which(lenv, prog):
 	if lenv['WINDOWS']: # we're not depending on Cygwin
 		return ''
 	return os.popen("which %s 2>/dev/null" % prog).read().strip()
-
+	
 def sub_config(file):
 	return ''
 
 class Configure:
-
-	def __init__(self, config):
-		for key in config.keys():
+	
+	def __init__(self, config=None):
+		if not config:
+			self.config = Environment.Environment()
+		else:
+			self.config = config
+		for key in config.m_table:
 			if key == 'modules':
 				self.modules = config[key].split()
 
@@ -67,10 +71,17 @@ class Configure:
 		filename = env.getValue('OS') + '.env'
 		env.store(filename)
 
+	
 	def setConfigHeader(self, header):
+		"""set a config header file"""
+		pass
+
+	def checkHeaders(self, header, headers):
+		"""find a header"""
 		pass
 
 	def checkFunction(self, function, header = None, language = None):
+		"""find a function"""
 		if not header:
 			header = """
 #ifdef __cplusplus
@@ -78,11 +89,45 @@ extern "C"
 #endif
 char %s();""" % function
 
-		
-	def checkHeaders(self, header, headers):
+	def checkProgram(self, file, path_list=None):
+		"""find an application"""
+		ret = find_program(self.config, file, path_list=None)
+		self.checkMessage('program',file,ret,ret)
+		return ret
+
+	def checkLibrary(self, header, headers):
+		"""find a library"""
 		pass
 
+	def checkTool(self,tool):
+		"""check if a waf tool is available"""
+		return self.config.detect(tool)
+			
+	def checkModule(self,tool):
+		"""check if a tool is given"""
+		pass
 
+	def error(lenv,module,str):
+		"""prints an error message"""
+		print "configuration error: " + module + " " + str
 
+	def store(lenv,file):
+		"""save config results into a cache file"""
+		return self.config.store(file)
+
+	def checkMessage(self,type,msg,state,option=''):
+		"""print an checking message. This function is used by other checking functions"""
+		str = 'checking for ' + type + ' ' + msg
+		if state:
+			str += ': found ' + option
+		else:
+			str += ': not found'
+		print str
+		
+
+	def detect(self,tool):
+		"""deprecated, replaced by checkTool"""
+		return self.config.detect(tool)
+	
 
 
