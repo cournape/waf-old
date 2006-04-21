b1d3a722fbb83f6846e98deb15d2ac430a6708ad
tnagy <tnagy@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2006-04-21 18:26:47 +0000

    Code factorization.
    git-svn-id: http://waf.googlecode.com/svn/trunk@207 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/ccroot.py
+++ wafadmin/Tools/ccroot.py
@@ -73,14 +73,85 @@ class ccroot(Object.genobj):
 		self.p_flag_vars = []
 		self.p_type_vars = []
 
-	# subclass me
-	def apply(self):
-		fatal('subclass method apply of ccroot')
+		self.m_type_initials = ''
 
 	# subclass me
 	def get_valid_types(self):
 		fatal('subclass method get_valid_types of ccroot')
 
+	# adding some kind of genericity is tricky
+	# subclass this method if it does not suit your needs
+	def apply(self):
+		trace("apply called for "+self.m_type_initials)
+		if not self.m_type in self.get_valid_types(): fatal('Invalid type for object: '+self.m_type_initials)
+
+		self.apply_lib_vars()
+		self.apply_type_vars()
+		self.apply_obj_vars()
+		self.apply_incpaths()
+
+		obj_ext = self.env[self.m_type+'_obj_ext'][0]
+		pre = self.m_type_initials
+
+		# get the list of folders to use by the scanners
+                # all our objects share the same include paths anyway
+                tree = Params.g_build.m_tree
+                dir_lst = { 'path_lst' : self._incpaths_lst }
+
+		lst = self.source.split()
+		for filename in lst:
+
+			node = self.m_current_path.find_node( filename.split(os.sep) )
+			if not node:
+				error("source not found "+filename)
+				print "source not found", self.m_current_path
+				sys.exit(1)
+
+			base, ext = os.path.splitext(filename)
+
+			fun = None
+			try:
+				fun = self.env['handlers_'+pre+'obj_'+ext]
+				#print "fun is", 'handlers_cppobj_'+ext, fun
+			except:
+				pass
+
+			if fun:
+				fun(self, node)
+				continue
+
+			if tree.needs_rescan(node):
+				tree.rescan(node, Scan.c_scanner, dir_lst)
+
+			# create the compilation task: cpp or cc
+			task = self.create_task(self.m_type_initials, self.env)
+
+			task.m_scanner = Scan.c_scanner
+			task.m_scanner_params = dir_lst
+
+			task.m_inputs  = self.file_in(filename)
+			task.m_outputs = self.file_in(base+obj_ext)
+			self.p_compiletasks.append(task)
+
+		# and after the objects, the remaining is the link step
+		# link in a lower priority (101) so it runs alone (default is 10)
+		global g_prio_link
+		if self.m_type=='staticlib': linktask = self.create_task(pre+'_link_static', self.env, g_prio_link)
+		else:                        linktask = self.create_task(pre+'_link', self.env, g_prio_link)
+		outputs = []
+		for t in self.p_compiletasks: outputs.append(t.m_outputs[0])
+		linktask.m_inputs  = outputs 
+		linktask.m_outputs = self.file_in(self.get_target_name())
+
+		self.m_linktask = linktask
+
+		if self.m_type != 'program':	
+			latask = self.create_task('fakelibtool', self.env, 200)
+			latask.m_inputs = linktask.m_outputs
+			latask.m_outputs = self.file_in(self.get_target_name('.la'))
+			self.m_latask = latask
+
+		self.apply_libdeps()
 
 	def get_target_name(self, ext=None):
 		return self.get_library_name(self.target, self.m_type, ext)
--- wafadmin/Tools/cpp.py
+++ wafadmin/Tools/cpp.py
@@ -35,6 +35,8 @@ class cppobj(ccroot.ccroot):
 		self.m_linktask=None
 		self.m_deps_linktask=[]
 
+		self.m_type_initials = 'cpp'
+
 		global g_cpp_flag_vars
 		self.p_flag_vars = g_cpp_flag_vars
 
@@ -43,77 +45,6 @@ class cppobj(ccroot.ccroot):
 
 	def get_valid_types(self):
 		return ['program', 'shlib', 'staticlib']
-
-	def apply(self):
-		trace("apply called for cppobj")
-		if not self.m_type in self.get_valid_types(): fatal('Trying to build a cpp file of unknown type')
-
-		self.apply_lib_vars()
-		self.apply_type_vars()
-		self.apply_obj_vars()
-		self.apply_incpaths()
-
-		obj_ext = self.env[self.m_type+'_obj_ext'][0]
-
-		# get the list of folders to use by the scanners
-                # all our objects share the same include paths anyway
-                tree = Params.g_build.m_tree
-                dir_lst = { 'path_lst' : self._incpaths_lst }
-
-		lst = self.source.split()
-		for filename in lst:
-
-			node = self.m_current_path.find_node( filename.split(os.sep) )
-			if not node:
-				error("source not found "+filename)
-				print "source not found", self.m_current_path
-				sys.exit(1)
-
-			base, ext = os.path.splitext(filename)
-
-			fun = None
-			try:
-				fun = self.env['handlers_cppobj_'+ext]
-				#print "fun is", 'handlers_cppobj_'+ext, fun
-			except:
-				pass
-
-			if fun:
-				fun(self, node)
-				continue
-
-			if tree.needs_rescan(node):
-				tree.rescan(node, Scan.c_scanner, dir_lst)
-
-			# create the task for the cpp file
-			cpptask = self.create_task('cpp', self.env)
-
-			cpptask.m_scanner = Scan.c_scanner
-			cpptask.m_scanner_params = dir_lst
-
-			cpptask.m_inputs  = self.file_in(filename)
-			cpptask.m_outputs = self.file_in(base+obj_ext)
-			self.p_compiletasks.append(cpptask)
-
-		# and after the cpp objects, the remaining is the link step
-		# link in a lower priority (101) so it runs alone (default is 10)
-		if self.m_type=='staticlib': linktask = self.create_task('cpp_link_static', self.env, ccroot.g_prio_link)
-		else:                        linktask = self.create_task('cpp_link', self.env, ccroot.g_prio_link)
-		cppoutputs = []
-		for t in self.p_compiletasks: cppoutputs.append(t.m_outputs[0])
-		linktask.m_inputs  = cppoutputs 
-		linktask.m_outputs = self.file_in(self.get_target_name())
-
-		self.m_linktask = linktask
-
-		if self.m_type != 'program':	
-			latask = self.create_task('fakelibtool', self.env, 200)
-			latask.m_inputs = linktask.m_outputs
-			latask.m_outputs = self.file_in(self.get_target_name('.la'))
-			self.m_latask = latask
-
-		self.apply_libdeps()
-
 
 # tool specific setup
 # is called when a build process is started 
