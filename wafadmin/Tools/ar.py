d1d0232019bf258922949d20a43541350444ee66
tnagy <tnagy@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2006-07-09 13:23:11 +0000

    Cleanup in ar.py.
    git-svn-id: http://waf.googlecode.com/svn/trunk@363 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/ar.py
+++ wafadmin/Tools/ar.py
@@ -6,41 +6,24 @@
 import os, sys
 import Utils, Configure, Action, Runner
 
-ar_vardeps = ['AR', 'RANLIB', 'ARFLAGS', 'RANLIBFLAGS']
-def ar_build(task):
-	#reldir = task.m_inputs[0].cd_to()
-
-	infiles = " ".join(  map(lambda a:a.bldpath(task.m_env), task.m_inputs)  )
-	outfile = " ".join(  map(lambda a:a.bldpath(task.m_env), task.m_outputs)  )
-
-	#infiles = task.m_inputs[0].bldpath()
-	#outfile = task.m_outputs[0].m_name
-
-	e=task.m_env
-	s = '%s %s %s %s && %s %s %s'
-	cmd = s % (e['AR'], e['ARFLAGS'], outfile, infiles, e['RANLIB'], e['RANLIBFLAGS'], outfile)
-	return Runner.exec_command(cmd)
+ar_str = '${AR} ${ARFLAGS} ${TGT} ${SRC} && ${RANLIB} ${RANLIBFLAGS} ${TGT}'
 
 def setup(env):
-	aract = Action.GenAction('cpp_link_static', ar_vardeps)
-	aract.m_function_to_run = ar_build
-
-	aract = Action.GenAction('cc_link_static', ar_vardeps)
-	aract.m_function_to_run = ar_build
+	Action.simple_action('cpp_link_static', ar_str)
+	Action.simple_action('cc_link_static', ar_str)
 
 def detect(conf):
 
 	comp = conf.checkProgram('ar', var='AR')
-	if not comp:
-		return 0;
+	if not comp: return 0;
 
 	ranlib = conf.checkProgram('ranlib', var='RANLIB')
-	if not ranlib:
-		return 0
+	if not ranlib: return 0
 
-	conf.env['AR']                = comp
-	conf.env['ARFLAGS']           = 'r'
-	conf.env['RANLIB']            = ranlib
-	conf.env['RANLIBFLAGS']       = ''
+	v = conf.env
+	v['AR']          = comp
+	v['ARFLAGS']     = 'r'
+	v['RANLIB']      = ranlib
+	v['RANLIBFLAGS'] = ''
 	return 1
 
