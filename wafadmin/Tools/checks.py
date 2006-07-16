813bf1ae5365b8fbd9a91990ba8e6245b426b957
tnagy <tnagy@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2006-07-16 12:36:16 +0000

    Added a new Check - not modular enough, but this will be resolved in the future.
    git-svn-id: http://waf.googlecode.com/svn/trunk@417 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/checks.py
+++ wafadmin/Tools/checks.py
@@ -25,7 +25,7 @@ int main()
 """
 
 def checkEndian(self, define='', pathlst=[]):
-	if define == '': define = 'HAVE_BIGENDIAN'
+	if define == '': define = 'IS_BIGENDIAN'
 
 	if self.isDefined(define): return self.getDefine(define)
 
@@ -47,6 +47,62 @@ def checkEndian(self, define='', pathlst=[]):
 	self.addDefine(define, is_big)
 	return is_big
 
+features_str = """
+#include <stdio.h>
+int am_big_endian()
+{
+        long one = 1;
+        return !(*((char *)(&one)));
+}
+
+int main()
+{
+  if (am_big_endian())
+     printf("bigendian=1\\n");
+  else
+     printf("bigendian=0\\n");
+
+  printf("int_size=%d\\n", sizeof(int));
+  printf("long_int_size=%d\\n", sizeof(long int));
+  printf("long_long_int_size=%d\\n", sizeof(long long int));
+  printf("double_size=%d\\n", sizeof(double));
+
+  return 0;
+}
+"""
+
+def checkFeatures(self, lst=[], pathlst=[]):
+
+	global endian
+	code = self.TryRun(features_str, pathlst=pathlst)
+
+	try:
+		t = Utils.to_hashtable(code)
+		is_big = int(t['bigendian'])
+	except:
+		error('endian test failed '+code)
+		is_big = 0
+		raise
+
+	if is_big: strbig = 'big endian'
+	else:      strbig = 'little endian'
+
+
+	self.checkMessageCustom('endianness', '', strbig)
+
+	self.checkMessageCustom('int size', '', t['int_size'])
+	self.checkMessageCustom('long int size', '', t['long_int_size'])
+	self.checkMessageCustom('long long int size', '', t['long_long_int_size'])
+	self.checkMessageCustom('double size', '', t['double_size'])
+
+	self.addDefine('IS_BIGENDIAN', is_big)
+	self.addDefine('INT_SIZE', int(t['int_size']))
+	self.addDefine('LONG_INT_SIZE', int(t['long_int_size']))
+	self.addDefine('LONG_LONG_INT_SIZE', int(t['long_long_int_size']))
+	self.addDefine('DOUBLE_SIZE', int(t['double_size']))
+
+	return is_big
+
 
 def setup(env):
 	# we provide no new action or builder
@@ -55,5 +111,6 @@ def setup(env):
 def detect(conf):
 	# attach the checks to the conf object
 	setattr(conf.__class__, checkEndian.__name__, checkEndian)
+	setattr(conf.__class__, checkFeatures.__name__, checkFeatures)
 	return 1
 
