d70d920b0ebd688f33160d4f1f613799d67c8ae8
tnagy <tnagy@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2006-07-29 22:18:58 +0000

    packaging, gnome
    git-svn-id: http://waf.googlecode.com/svn/trunk@470 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- demos/gnome/src/hello.c
+++ demos/gnome/src/hello.c
@@ -97,7 +97,7 @@ main (int argc, char **argv)
 
   program = gnome_program_init (PACKAGE, VERSION, LIBGNOMEUI_MODULE,
 				argc, argv, 
-				GNOME_PARAM_GOPTION_CONTEXT, context,
+//				GNOME_PARAM_GOPTION_CONTEXT, context,
 				GNOME_PARAM_APP_DATADIR,DATADIR,
 				NULL);
 
--- wafadmin/Tools/gnome.py
+++ wafadmin/Tools/gnome.py
@@ -2,11 +2,44 @@
 # encoding: utf-8
 # Thomas Nagy, 2006 (ita)
 
-# TODO more to come
+import os
+import Object, Action
+
+# translations
+class gnome_translations(Object.genobj):
+	def __init__(self, appname):
+		Object.genobj.__init__(self, 'other')
+		self.m_tasks=[]
+		self.m_appname = appname
+	def apply(self):
+		for file in self.m_current_path.m_files:
+			try:
+				base, ext = os.path.splitext(file.m_name)
+				if ext != '.po': continue
+
+				task = self.create_task('po', self.env, 2)
+				task.set_inputs(file)
+				task.set_outputs(file.change_ext('.gmo'))
+				self.m_tasks.append(task)
+			except: pass
+	def install(self):
+		destfilename = self.m_appname+'.mo'
+
+		current = Params.g_build.m_curdirnode
+		for file in self.m_current_path.m_files:
+			lang, ext = os.path.splitext(file.m_name)
+			if ext != '.po': continue
+
+			node = self.m_current_path.find_node( (lang+'.gmo').split('/') )
+			orig = node.relpath_gen(current)
+
+			destfile = os.sep.join([lang, 'LC_MESSAGES', destfilename])
+			Common.install_as('GNOME_LOCALE', destfile, orig, self.env)
+
 
 def setup(env):
-	# nothing to do yet
-	return
+	Action.simple_action('po', '${POCOM} -o ${TGT} ${SRC}', color='BLUE')
+	Object.register('gnome_translations', gnome_translations)
 
 def detect(env):
 	# nothing to do yet
