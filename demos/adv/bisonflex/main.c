3fbdd32fede718c69bbd289a84c02d4128f10e15
tnagy <tnagy@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2006-08-04 22:37:24 +0000

    Trying to make it compatible c and c++.
    git-svn-id: http://waf.googlecode.com/svn/trunk@535 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- demos/adv/bisonflex/main.c
+++ demos/adv/bisonflex/main.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <iostream>
-
-//using namespace std;
+/* this file should work in both c and c++ */
 extern int yyparse();
 
 int yyerror (char const *a)
 {
-  printf("yyerror: (%s)\n", a);
-  return 1;
+	printf("yyerror: (%s)\n", a);
+	return 1;
 }
 
 int main(int argc, char *argv[])
 {
-  int yy;
-  yy = yyparse();
-  if (yy != 0)
-  {
-	  printf("Syntax or parse error %i. Aborting.\n", yy);
-	  return 1;
-  }
-  else{
-	  printf("Success.\n");
-  }
+	int yy;
+	yy = yyparse();
+	if (yy != 0)
+	{
+		printf("Syntax or parse error %i. Aborting.\n", yy);
+		return 1;
+	}
+	else{
+		printf("Success.\n");
+	}
+	return 0;
 }
