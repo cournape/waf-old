b73e1d7370581c92a2ddf8cdf6a64e220c3ca5f0
tnagy <tnagy@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2006-08-08 20:26:23 +0000

    Latex update: * let the demo actually run * run latex as many times as necessary (tables of content, ..): until file.aux is unchanged
    git-svn-id: http://waf.googlecode.com/svn/trunk@567 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/Tex.py
+++ wafadmin/Tools/Tex.py
@@ -12,15 +12,47 @@ latex_vardeps  = ['LATEX', 'LATEXFLAGS', 'LATEX_ST']
 def latex_build(task):
 	com = task.m_env['LATEX']
 	node = task.m_inputs[0]
-	reldir  = node.cd_to()
-	uppath = "".join(node.m_parent.invrelpath(Params.g_build.m_bldnode))
-	srcfile = os.path.join(uppath, node.bldpath())
-	#print srcfile
-	#sys.exit(0)
+	reldir  = node.cd_to(task.m_env)
 
-	cmd = 'cd %s && %s %s' % (reldir, com, srcfile)
-	return Runner.exec_command(cmd)
+	srcfile = node.srcpath(task.m_env)
 
+	lst = []
+	for c in reldir.split(os.sep):
+		if c: lst.append('..')
+	lst.append(srcfile)
+
+	sr = os.sep.join(lst)
+	
+	aux_node = node.change_ext('.aux')
+
+	hash     = ''
+	old_hash = ''
+
+	i = 0
+	while i < 10:
+		# prevent against infinite loops
+		i += 1
+
+		# watch the contents of file.aux
+		old_hash = hash
+		try:
+			hash = Utils.h_md5_file(aux_node.abspath(task.m_env))
+		except:
+			pass
+
+		# debug
+		#print "hash is, ", hash, " ", old_hash
+
+		# stop if file.aux does not change anymore
+		if hash and hash == old_hash: break
+
+		# run the command
+		cmd = 'cd %s && %s %s' % (reldir, com, sr)
+		ret = Runner.exec_command(cmd)
+		if ret: return ret
+
+	# 0 means no error
+	return 0
 
 g_texobjs=['latex','tex','bibtex','dvips','dvipdf']
 class texobj(Object.genobj):
@@ -40,9 +72,12 @@ class texobj(Object.genobj):
 			base, ext = os.path.splitext(filename)
 			if not ext=='.tex': continue
 
+			node = self.m_current_path.find_node( filename.split(os.sep) )
+			if not node: fatal('cannot find %s' % filename)
+
 			task = self.create_task('latex', self.env, 2)
-			task.m_inputs = [ self.get_mirror_node( self.m_current_path, base+'.tex') ]
-			task.m_outputs = [ self.get_mirror_node( self.m_current_path, base+'.dvi') ]
+			task.set_inputs(node)
+			task.set_outputs(node.change_ext('.dvi'))
 
 def detect(conf):
 	v = conf.env
