a7d123c717919c61f2d064c92032bc2780916666
tnagy <tnagy@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2006-08-16 22:02:44 +0000

    Latex testcase: bibtex, makeindex and table of contents.
    git-svn-id: http://waf.googlecode.com/svn/trunk@584 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/Tex.py
+++ wafadmin/Tools/Tex.py
@@ -4,37 +4,94 @@
 
 # found is 1, not found is 0
 
-import os, sys
+import os, sys, re
 import Utils, Params, Action, Object, Runner
+from Params import error, warning
+
+g_bibtex_re = re.compile('bibdata', re.M)
 
 latex_vardeps  = ['LATEX']
 def latex_build(task):
-	com = task.m_env['LATEX']
-	node = task.m_inputs[0]
-	reldir  = node.cd_to(task.m_env)
+	env = task.m_env
 
-	srcfile = node.srcpath(task.m_env)
+	com = env['LATEX']
+	node = task.m_inputs[0]
+	reldir  = node.cd_to(env)
+
+	srcfile = node.srcpath(env)
 
 	lst = []
 	for c in reldir.split(os.sep):
 		if c: lst.append('..')
 	sr = os.sep.join(lst + [srcfile])
-	sr2 = os.sep.join(lst + [node.m_parent.srcpath(task.m_env)])
+	sr2 = os.sep.join(lst + [node.m_parent.srcpath(env)])
 
 	aux_node = node.change_ext('.aux')
+	idx_node = node.change_ext('.idx')
 
 	hash     = ''
 	old_hash = ''
 
+
+
+	# TODO remove
+	#import Runner
+	#Runner.g_quiet = 1
+
+
+	nm = aux_node.m_name
+	docuname = nm[ : len(nm) - 4 ] # 4 is the size of ".aux"
+
+	# mandatory first pass
+	latex_compile_cmd = 'cd %s && TEXINPUTS=%s:$TEXINPUTS %s %s' % (reldir, sr2, com, sr)
+	warning('first pass on latex')
+	ret = Runner.exec_command(latex_compile_cmd)
+	if ret: return ret
+
+	# look in the .aux file if there is a bibfile to process
+	try:
+		file = open(aux_node.abspath(env), 'r')
+		fo = g_bibtex_re.findall(file.read())
+		file.close()
+
+		# yes, there is a .aux file to process
+		if fo:
+			bibtex_compile_cmd = 'cd %s && BIBINPUTS=%s:$BIBINPUTS %s %s' % (reldir, sr2, env['BIBTEX'], docuname)
+
+			warning('calling bibtex')
+			ret = Runner.exec_command(bibtex_compile_cmd)
+			if ret:
+				error('error when calling bibtex %s' % bibtex_compile_cmd)
+				return ret
+
+	except:
+		error('erreur bibtex scan')
+		pass
+
+	# look on the filesystem if there is a .idx file to process
+	try:
+		idx_path = idx_node.abspath(env)
+		os.stat(idx_path)
+		
+		makeindex_compile_cmd = 'cd %s && %s %s' % (reldir, env['MAKEINDEX'], idx_path)
+		warning('calling makeindex')
+		ret = Runner.exec_command(makeindex_compile_cmd)
+		if ret:
+			error('error when calling makeindex %s' % makeindex_compile_cmd)
+			return ret
+	except:
+		error('erreur file.idx scan')
+		pass
+
 	i = 0
 	while i < 10:
-		# prevent against infinite loops
+		# prevent against infinite loops - one never knows
 		i += 1
 
 		# watch the contents of file.aux
 		old_hash = hash
 		try:
-			hash = Utils.h_md5_file(aux_node.abspath(task.m_env))
+			hash = Utils.h_md5_file(aux_node.abspath(env))
 		except:
 			pass
 
@@ -45,9 +102,11 @@ def latex_build(task):
 		if hash and hash == old_hash: break
 
 		# run the command
-		cmd = 'cd %s && TEXINPUTS=%s:$TEXINPUTS %s %s' % (reldir, sr2, com, sr)
-		ret = Runner.exec_command(cmd)
-		if ret: return ret
+		warning('calling latex')
+		ret = Runner.exec_command(latex_compile_cmd)
+		if ret:
+			error('error when calling latex %s' % latex_compile_cmd)
+			return ret
 
 	# 0 means no error
 	return 0
@@ -107,6 +166,9 @@ def detect(conf):
 
 	v['DVIPDF']      = conf.find_program('dvipdf')
 	v['DVIPDFFLAGS'] = ''
+
+	v['MAKEINDEX']   = conf.find_program('makeindex')
+
 	return 1
 
 def setup(env):
