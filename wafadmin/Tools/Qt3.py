d189202f47256ab2e15ddc748810dd61378ffd79
tnagy <tnagy@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2006-10-24 17:32:25 +0000

    Another patch by Carlos: Allow Qt3.py and Qt4.py to be used at the same time
    git-svn-id: http://waf.googlecode.com/svn/trunk@925 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/Qt3.py
+++ wafadmin/Tools/Qt3.py
@@ -5,6 +5,8 @@
 Qt3 support
 
 If QTDIR is given (absolute path), the configuration will look in it first
+
+This module also demonstrates how to add tasks dynamically (when the build has started)
 """
 
 import os, sys
@@ -13,10 +15,10 @@ import Action, Params, Object, Task, Utils
 from Params import error, fatal
 from Params import set_globals, globals
 
-set_globals('MOC_H', ['.hpp', '.hxx', '.hh', '.h'])
-set_globals('UI_EXT', ['.ui'])
+set_globals('QT3_MOC_H', ['.hpp', '.hxx', '.hh', '.h'])
+set_globals('QT3_UI_EXT', ['.ui'])
 
-uic_vardeps = ['QT_UIC', 'UIC_FLAGS', 'UIC_ST']
+uic_vardeps = ['QT3_UIC', 'QT3_UIC_FLAGS', 'QT3_UIC_ST']
 
 class MTask(Task.Task):
 	"A cpp task that may create a moc task dynamically"
@@ -31,7 +33,7 @@ class MTask(Task.Task):
 		tree = Params.g_build
 		parn = self.parent
 		node = self.m_inputs[0]
-
+		
 		# scan the .cpp files and find if there is a moc file to run
 		if tree.needs_rescan(node, parn.env):
 			ccroot.g_c_scanner.do_scan(node, parn.env, hashparams = self.m_scanner_params)
@@ -54,11 +56,11 @@ class MTask(Task.Task):
 			mocfiles.append(d)
 
 			# find the extension - this search is done only once
-			if Params.g_options.qt_header_ext:
-				ext = Params.g_options.qt_header_ext
+			if Params.g_options.qt3_header_ext:
+				ext = Params.g_options.qt3_header_ext
 			else:
 				path = node.m_parent.srcpath(parn.env)
-				for i in globals('MOC_H'):
+				for i in globals('QT3_MOC_H'):
 					try:
 						os.stat(Utils.join_path(path,base2+i))
 						ext = i
@@ -73,16 +75,17 @@ class MTask(Task.Task):
 			tree.m_depends_on[variant][m_node] = h_node
 
 			# create the task
-			task = parn.create_task('moc_hack', parn.env)
+			task = parn.create_task('moc3_hack', parn.env)
 			task.set_inputs(h_node)
 			task.set_outputs(m_node)
 			moctasks.append(task)
 		# look at the file inputs, it is set right above
 		for d in tree.m_depends_on[variant][node]:
 			deps = tree.m_depends_on[variant]
+			
 			name = d.m_name
 			if name[-4:]=='.moc':
-				task = parn.create_task('moc_hack', parn.env)
+				task = parn.create_task('moc3_hack', parn.env)
 				task.set_inputs(tree.m_depends_on[variant][d])
 				task.set_outputs(d)
 				moctasks.append(task)
@@ -93,7 +96,7 @@ class MTask(Task.Task):
 
 def create_uic_task(self, node):
 	"hook for uic tasks"
-	uictask = self.create_task('uic3', self.env, 6)
+	uictask = self.create_task('qt3_uic3', self.env, 6)
 	uictask.m_inputs    = [node]
 	uictask.m_outputs   = [node.change_ext('.h')]
 
@@ -114,8 +117,8 @@ class qt3obj(cpp.cppobj):
 			task = MTask(type, env, self, nice)
 		elif type == 'cpp_ui':
 			task = Task.Task('cpp', env, nice)
-		elif type == 'moc_hack': # add a task while the build has started
-			task = Task.Task('moc', env, nice, normal=0)
+		elif type == 'moc3_hack': # add a task while the build has started
+			task = Task.Task('qt3_moc', env, nice, normal=0)
 			generator = Params.g_build.m_generator
 			#generator.m_outstanding.append(task)
 			generator.m_outstanding = [task] + generator.m_outstanding
@@ -128,11 +131,11 @@ class qt3obj(cpp.cppobj):
 		return task
 
 def setup(env):
-	Action.simple_action('moc', '${QT_MOC} ${MOC_FLAGS} ${SRC} ${MOC_ST} ${TGT}', color='BLUE')
-	Action.simple_action('uic3', '${QT_UIC} ${SRC} -o ${TGT}', color='BLUE')
+	Action.simple_action('qt3_moc', '${QT3_MOC} ${QT3_MOC_FLAGS} ${SRC} ${QT3_MOC_ST} ${TGT}', color='BLUE')
+	Action.simple_action('qt3_uic3', '${QT3_UIC} ${SRC} -o ${TGT}', color='BLUE')
 	Object.register('qt3', qt3obj)
 
-	try: env.hook('qt3', 'UI_EXT', create_uic_task)
+	try: env.hook('qt3', 'QT3_UI_EXT', create_uic_task)
 	except: pass
 
 def detect_qt3(conf):
@@ -140,55 +143,55 @@ def detect_qt3(conf):
 	env = conf.env
 	opt = Params.g_options
 
-	try: qtlibs = opt.qtlibs
-	except: qtlibs=''
+	try: qt3libs = opt.qt3libs
+	except: qt3libs=''
 
-	try: qtincludes = opt.qtincludes
-	except: qtincludes=''
+	try: qt3includes = opt.qt3includes
+	except: qt3includes=''
 
-	try: qtbin = opt.qtbin
-	except: qtbin=''
+	try: qt3bin = opt.qt3bin
+	except: qt3bin=''
 
-	# if qtdir is given - helper for finding qtlibs, qtincludes and qtbin
-	try: qtdir = opt.qtdir
-	except: qtdir=''
+	# if qt3dir is given - helper for finding qt3libs, qt3includes and qt3bin
+	try: qt3dir = opt.qt3dir
+	except: qt3dir=''
 
-	if not qtdir: qtdir = os.environ.get('QTDIR', '')
+	if not qt3dir: qt3dir = os.environ.get('QTDIR', '')
 
 	# Debian/Ubuntu support ('/usr/share/qt3/')
 	# Gentoo support ('/usr/qt/3')
-	if not qtdir:
+	if not qt3dir:
 		candidates = ['/usr/share/qt3/', '/usr/qt/3/']
-
+		
 		for candidate in candidates:
 			if os.path.exists(candidate):
-				qtdir = candidate
+				qt3dir = candidate
 				break
 
-	if qtdir and qtdir[len(qtdir)-1] != '/':
-		qtdir += '/'
+	if qt3dir and qt3dir[len(qt3dir)-1] != '/':
+		qt3dir += '/'
 
-	if qtdir:
-		env['QT3_DIR'] = qtdir
-
-	if not qtdir:
-		if qtlibs and qtincludes and qtbin:
+	if qt3dir:
+		env['QT3_DIR'] = qt3dir
+		
+	if not qt3dir:
+		if qt3libs and qt3includes and qt3bin:
 			Params.pprint("YELLOW", "No valid qtdir found; using the specified qtlibs, qtincludes and qtbin params");
 		else:
 			fatal("Cannot find a valid qtdir, and not enough parameters are set; either specify a qtdir, or qtlibs, qtincludes and qtbin params")
 
 	# check for the qtbinaries
-	if not qtbin: qtbin = qtdir + 'bin/'
+	if not qt3bin: qt3bin = qt3dir + 'bin/'
 
-	# If a qtbin (or a qtdir) param was given, test the version-neutral names first
-	if qtbin:
+	# If a qt3bin (or a qt3dir) param was given, test the version-neutral names first
+	if qt3bin:
 		moc_candidates = ['moc', 'moc-qt3']
 		uic_candidates = ['uic', 'uic3', 'uic-qt3']
 	else:
 		moc_candidates = ['moc-qt3', 'moc']
 		uic_candidates = ['uic-qt3', 'uic3', 'uic']
 
-	binpath = [qtbin] + os.environ['PATH'].split(':')
+	binpath = [qt3bin] + os.environ['PATH'].split(':')
 	def find_bin(lst, var):
 		for f in lst:
 			ret = conf.find_program(f, path_list=binpath)
@@ -196,35 +199,38 @@ def detect_qt3(conf):
 				env[var]=ret
 				return ret
 
-	if not find_bin(uic_candidates, 'QT_UIC'):
+	Params.pprint("GREEN", "Trying to find uic");
+	if not find_bin(uic_candidates, 'QT3_UIC'):
 		fatal("uic not found!")
-	version = os.popen(env['QT_UIC'] + " -version 2>&1").read().strip()
+	version = os.popen(env['QT3_UIC'] + " -version 2>&1").read().strip()
 	version = version.replace('Qt User Interface Compiler ','')
 	version = version.replace('User Interface Compiler for Qt', '')
 	if version.find(" 3.") == -1:
 		conf.check_message('uic version', '(not a 3.x uic)', 0, option='(%s)'%version)
 		sys.exit(1)
-	conf.check_message('uic version', '', 1, option='%s'%version)
+	conf.check_message('uic version', '', 1, option='(%s)'%version)
 
-	if not find_bin(moc_candidates, 'QT_MOC'):
+	Params.pprint("GREEN", "Trying to find moc");
+	if not find_bin(moc_candidates, 'QT3_MOC'):
 		fatal("moc not found!")
 
-	env['UIC_ST'] = '%s -o %s'
-	env['MOC_ST'] = '-o'
+	env['QT3_UIC_ST'] = '%s -o %s'
+	env['QT3_MOC_ST'] = '-o'
 
-	if not qtincludes: qtincludes = qtdir + 'include/'
-	if not qtlibs: qtlibs = qtdir + 'lib/'
-
+	if not qt3includes: qt3includes = qt3dir + 'include/'
+	if not qt3libs: qt3libs = qt3dir + 'lib/'
+	
 	# check for the qt-mt package
+	Params.pprint("GREEN", "Trying to use the qt-mt pkg-config package");
 	pkgconf = conf.create_pkgconfig_configurator()
 	pkgconf.name = 'qt-mt'
 	pkgconf.uselib = 'QT3'
-	pkgconf.path = qtlibs
+	pkgconf.path = qt3libs
 	if not pkgconf.run():
 		Params.pprint("YELLOW", "qt-mt package not found - trying to enumerate paths & flags manually");
-
+		
 		# check for the qt includes first
-		lst = [qtincludes, '/usr/qt/3/include', '/usr/include/qt3', '/opt/qt3/include', '/usr/local/include', '/usr/include']
+		lst = [qt3includes, '/usr/qt/3/include', '/usr/include/qt3', '/opt/qt3/include', '/usr/local/include', '/usr/include']
 		headertest = conf.create_header_enumerator()
 		headertest.name = 'qt.h'
 		headertest.path = lst
@@ -233,7 +239,7 @@ def detect_qt3(conf):
 		env['CPPPATH_QT3'] = ret
 
 		# now check for the qt libs
-		lst = [qtlibs, '/usr/qt/3/lib', '/usr/lib/qt3', '/opt/qt3/lib', '/usr/local/lib', '/usr/lib']
+		lst = [qt3libs, '/usr/qt/3/lib', '/usr/lib/qt3', '/opt/qt3/lib', '/usr/local/lib', '/usr/lib']
 		libtest = conf.create_library_enumerator()
 		libtest.name = 'qt-mt'
 		libtest.path = lst
@@ -243,11 +249,11 @@ def detect_qt3(conf):
 		env['LIB_QT3'] = 'qt-mt'
 
 	env['QT3_FOUND'] = 1
-
+		
 	# rpath settings
-	# TODO Carlos: Check if this works in darwin
+	# TODO: Check if this works in darwin
 	try:
-		if Params.g_options.want_rpath:
+		if Params.g_options.want_rpath_qt3:
 			env['RPATH_QT3']=['-Wl,--rpath='+qtlibs]
 	except:
 		pass
@@ -258,20 +264,20 @@ def detect(conf):
 	return 0
 
 def set_options(opt):
-	try: opt.add_option('--want-rpath', type='int', default=1, dest='want_rpath', help='set rpath to 1 or 0 [Default 1]')
+	try: opt.add_option('--want-rpath-qt3', type='int', default=1, dest='want_rpath_qt3', help='set rpath to 1 or 0 [Default 1]')
 	except: pass
 
-	opt.add_option('--header-ext',
+	opt.add_option('--header-ext-qt3',
 		type='string',
 		default='',
 		help='header extension for moc files',
-		dest='qt_header_ext')
+		dest='qt3_header_ext')
 
 	qtparams = {}
-	qtparams['qtdir'] = 'manual QTDIR specification (autodetected)'
-	qtparams['qtbin'] = 'path to the Qt3 binaries (moc, uic) (autodetected)'
-	qtparams['qtincludes'] = 'path to the Qt3 includes (autodetected)'
-	qtparams['qtlibs'] = 'path to the Qt3 libraries (autodetected)'
+	qtparams['qt3dir'] = 'manual Qt3 QTDIR specification (autodetected)'
+	qtparams['qt3bin'] = 'path to the Qt3 binaries (moc, uic) (autodetected)'
+	qtparams['qt3includes'] = 'path to the Qt3 includes (autodetected)'
+	qtparams['qt3libs'] = 'path to the Qt3 libraries (autodetected)'
 	for name, desc in qtparams.iteritems():
 		opt.add_option('--'+name, type='string', default='', help=desc, dest=name)
 
