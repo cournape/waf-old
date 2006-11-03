e5bb3f7837175000b8d938995b5ded018df126a3
dv7777 <dv7777@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2006-11-03 03:02:28 +0000

    In Configure.py: added message variable to enumerator_base for custom error messages on enumeration/configuration failure (the message variable will be ignored if its empty, which is the default state). demos/adv shows how to use it; see the kdecore autodetection for more.
    Fixed fatal() in Params.py to output the "(error raised in module <modulename>)" line only when -v or -vv are used.
    
    
    
    git-svn-id: http://waf.googlecode.com/svn/trunk@940 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Params.py
+++ wafadmin/Params.py
@@ -166,7 +166,10 @@ def error(msg):
 
 def fatal(msg):
 	module = __get_module()
-	pprint('RED', '%s \n (error raised in module %s)' % (msg, module))
+	if g_verbose > 0:
+		pprint('RED', '%s \n (error raised in module %s)' % (msg, module))
+	else:
+		pprint('RED', '%s' % msg)
 	sys.exit(1)
 
 def vsig(s):
