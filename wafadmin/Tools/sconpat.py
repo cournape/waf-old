6b04f27681c0916aa3335d64b0e2f997e592d560
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2007-01-17 17:26:03 +0000

    Scons compatibility .. just a start.
    git-svn-id: http://waf.googlecode.com/svn/trunk@1162 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/sconpat.py
+++ wafadmin/Tools/sconpat.py
@@ -2,30 +2,66 @@
 # encoding: utf-8
 # Thomas Nagy, 2006 (ita)
 
-import Utils, Configure, Action, md5
+import md5
+import Utils, Configure, Action, Task, Params
 from Params import error, fatal
 
 class sconpat_error:
 	pass
 
-# a nice way to hide parameters when a class would do it
+class Builder_class:
+	def __init__(self):
+		self.action = None
+		self.generator = None
+	def init(self, **kw):
+		if kw.has_key('generator') and kw.has_key('action'):
+			raise sconpat_error, 'do not mix action and generator in a builder'
+
+		if kw.has_key('action'):
+
+			a = kw['action'].replace('$SOURCES', '${SRC}')
+			a = a.replace('$TARGETS', '${TGT}')
+			a = a.replace('$TARGET', '${TGT[0].abspath(env)}')
+			a = a.replace('$SOURCE', '${SRC[0].abspath(env)}')
+
+			m = md5.new()
+			m.update(a)
+			key = m.hexdigest()
+
+			Action.simple_action(key, a, kw.get('color', 'GREEN'))
+			self.action=key
+	def apply(self, target, source, **kw):
+		#print "Builder_class apply called"
+		#print kw['env']
+		#print target
+		#print source
+
+		curdir = Params.g_build.m_curdirnode
+
+		t = Task.Task(self.action, kw['env'], 10)
+		t.set_inputs(curdir.find_source(source, create=1))
+		t.set_outputs(curdir.find_build(target, create=1))
+
 def Builder(**kw):
-	if kw.has_key('generator') and kw.has_key('action'):
-		raise sconpat_error, 'do not mix action and generator in a builder'
+	ret = Builder_class()
+	ret.init(**kw)
+	return ret
 
-	if kw.has_key('action'):
+def Environment(**kw):
+	import Environment
+	ret = Environment.Environment()
+	if kw.has_key('BUILDERS'):
+		bd = kw['BUILDERS']
+		for k in bd:
+			# store the builder name on the builder
+			bd[k].name = k
 
-		a = kw['action'].replace('$SOURCES', '${SRC}')
-		a = a.replace('$TARGETS', '${TGT}')
-		a = a.replace('$TARGET', '${TGT[0]}')
-		a = a.replace('$SOURCE', '${SRC[0]}')
+			def miapply(self, *lst, **kw):
+				if not 'env' in kw: kw['env']=ret
+				bd[k].apply(*lst, **kw)
 
-		m = md5.new()
-		m.update(a)
-		key = m.hexdigest()
-
-		Action.simple_action(key, a, kw.get('color', 'GREEN'))
-		kw['action']=key
+			ret.__class__.__dict__[k]=miapply
+	return ret
 
 def setup(env):
 	pass
