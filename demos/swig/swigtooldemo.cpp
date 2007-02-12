51f00c4e9bf7eba27bbc0e39638eef18353f094e
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2007-02-12 23:16:21 +0000

    Slightly more complicated example..
    git-svn-id: http://waf.googlecode.com/svn/trunk@1287 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- demos/swig/swigtooldemo.cpp
+++ demos/swig/swigtooldemo.cpp
@@ -1,8 +1,25 @@
+
+#include "Python.h"
 #include "swigtooldemo.hpp"
+
+extern "C"
+{
+	void init_swigdemo(void);
+}
 
 TestClass* TestClass::_instance = 0;
 
 int main()
 {
-    return 0;
+        Py_Initialize();
+        init_swigdemo();
+
+        /*FILE* file_py;
+        file_py = fopen(i_oFile.toLocal8Bit(), "r");
+        PyRun_SimpleFile(file_py, i_oFile.toLocal8Bit());
+        fclose(file_py);
+	*/
+	PyRun_SimpleString("import _swigdemo\nprint 'hey pfo'\nprint _swigdemo.instance()");
+
+        Py_Finalize();
 }
