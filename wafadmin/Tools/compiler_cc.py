eaeccf67bd1a3d33618bf22c5e7612a1ea12f8da
jahn.matthias <jahn.matthias@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2007-03-11 15:58:55 +0000

    - add a abstract cc compiler tool that selects depending to the plattform the corect c-compiler - changed an example to use the new compiler_cc tool (this builds well with the suncc on sunos 5)
    git-svn-id: http://waf.googlecode.com/svn/trunk@1309 f0382ac9-c320-0410-b3f0-b508d59f5a85

---
+++ wafadmin/Tools/compiler_cc.py
@@ -0,0 +1,99 @@
+#! /usr/bin/env python
+# encoding: utf-8
+# Matthias Jahn <jahn.matthias@freenet.de>, 2007 (pmarat)
+
+import os, sys, imp, types
+import optparse
+import Utils, Action, Params, checks, Configure
+
+def __detect_platform():
+	"""Stolen from scons"""
+	osname = os.name
+	if osname == 'java':
+		osname = os._osType
+	if osname == 'posix':
+		if sys.platform == 'cygwin':
+			return 'cygwin'
+		if str.find(sys.platform, 'linux') != -1:
+			return 'linux'
+		if str.find(sys.platform, 'irix') != -1:
+			return 'irix'
+		if str.find(sys.platform, 'sunos') != -1:
+			return 'sunos'
+		if str.find(sys.platform, 'hp-ux') != -1:
+			return 'hpux'
+		if str.find(sys.platform, 'aix') != -1:
+			return 'aix'
+		if str.find(sys.platform, 'darwin') != -1:
+			return 'darwin'
+		return 'posix'
+	elif os.name == 'os2':
+		return 'os2'
+	else:
+		return sys.platform
+
+def __list_possible_compiler():
+	plattform = __detect_platform()
+	c_compiler = {
+		"win32": ['msvc', 'mingw', 'gcc'],
+		"cygwin": ['gcc'],
+		"darwin": ['gcc'],
+		"aix5": ['gcc'],
+		"linux": ['gcc', 'suncc'],
+		"sunos": ['suncc', 'gcc'],
+		"irix": ['gcc'],
+		"hpux":['gcc'],
+		"default": ['gcc']
+	}
+	try:
+		return(c_compiler[plattform])
+	except:
+		return(c_compiler["default"])
+		
+	
+def setup(env):
+	pass
+
+def detect(conf):
+	test_for_compiler = Params.g_options.check_c_compiler
+	for c_compiler in test_for_compiler.split():
+		if conf.check_tool(c_compiler):
+			conf.check_message("%s" %c_compiler, '', True)
+			return (1)
+		conf.check_message("%s" %c_compiler, '', False)
+	return (0)
+
+def set_options(opt):
+	test_for_compiler = str(" ").join(__list_possible_compiler())
+	cc_compiler_opts = opt.parser.add_option_group("C Compiler Options")
+	try:
+		cc_compiler_opts.add_option('--check-c-compiler', default="%s" % test_for_compiler,
+			help='On this Plattform (%s) following C-Compiler will be checked default: "%s"' % 
+								(__detect_platform(), test_for_compiler),
+			dest="check_c_compiler")
+	except optparse.OptionConflictError:
+		# the g++ tool might have added that option already
+		pass
+
+	def l_tool_options(opts, tool, tooldir=None):
+		if type(tool) is types.ListType:
+			for i in tool: self.tool_options(i, tooldir)
+			return
+
+		if not tooldir: tooldir = Params.g_tooldir
+		tooldir = Utils.to_list(tooldir)
+		try:
+			file,name,desc = imp.find_module(tool, tooldir)
+		except:
+			error("no tool named '%s' found" % tool)
+			return
+		module = imp.load_module(tool,file,name,desc)
+		try:
+			module.set_options(opts)
+		except:
+			warning("tool %s has no function set_options or set_options failed" % tool)
+			pass
+
+	for c_compiler in test_for_compiler.split():
+		l_tool_options(cc_compiler_opts, '%s' % c_compiler)
+
