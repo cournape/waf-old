cc8dff13b815f3050bd450f72df6ddbd9a472d69
jahn.matthias <jahn.matthias@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2007-07-06 12:28:08 +0000

    add old scheduler test and fix the execution for this
    git-svn-id: http://waf.googlecode.com/svn/trunk@1439 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Test.py
+++ wafadmin/Test.py
@@ -4,17 +4,21 @@
 
 "Some waf tests - most are obsolete"
 
-import Params
+import Params, os
 
 def info(msg):
 	Params.pprint('CYAN', msg)
 
-def testname(file):
-	return open('test/'+file, 'r')
+def testname(file, tests_dir='test'):
+	test_file=os.path.join(tests_dir, file)
+	return open(test_file, 'r')
 
 def run_tests():
 	from test import build_dir
 	build_dir.run_tests()
+	for i in ['dist','configure','clean','distclean','make','install','doc']:
+		Params.g_commands[i]=0
+	exec testname('scheduler.tst', os.path.join('wafadmin', 'test'))
 
 if __name__ == "__main__":
 
