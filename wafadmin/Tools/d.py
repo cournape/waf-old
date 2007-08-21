a6ad653d89b0e2b9f0b01d932fe026aff5e3cc06
dv7777 <dv7777@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2007-08-21 20:52:06 +0000

    alpha D support added
    git-svn-id: http://waf.googlecode.com/svn/trunk@1479 f0382ac9-c320-0410-b3f0-b508d59f5a85

---
+++ wafadmin/Tools/d.py
@@ -0,0 +1,179 @@
+#! /usr/bin/env python
+# encoding: utf-8
+# Carlos Rafael Giani, 2007 (dv)
+
+import os, sys
+import optparse
+import Object, Utils, Action, Params, checks, Configure
+
+
+class dobj(Object.genobj):
+
+	s_default_ext = ['.d', '.di', '.D']
+	def __init__(self, type='program'):
+		Object.genobj.__init__(self, type)
+
+		self.dflags = ''
+		self.importpaths = ''
+		self.uselib = ''
+		self.uselib_local = ''
+
+
+	def detect(conf):
+		return 1
+
+
+	def apply(self):
+
+		#initialization
+		if self.m_type == 'objects':
+			type = 'program'
+		else:
+			type = self.m_type
+		
+		env = self.env
+		dpath_st         = env['DPATH_ST']
+		lib_st           = env['DLIB_ST']
+		libpath_st       = env['DLIBPATH_ST']
+
+
+		importpaths = []
+		libpaths = []
+		libs = []
+
+
+		linktask = self.create_task('d_link', self.env, 101)
+
+		# go through the local uselibs
+		for local_uselib in self.to_list(self.uselib_local):
+			y = Object.name_to_obj(local_uselib)
+			if not y: continue
+
+			if not y.m_posted: y.post()
+
+			if y.m_type == 'shlib':
+				libs = libs + [y.target]
+			elif y.m_type == 'staticlib':
+				libs = libs + [y.target]
+			elif y.m_type == 'objects':
+				pass
+			else:
+				error('%s has unknown object type %s, in apply_lib_vars, uselib_local.'
+				      % (y.name, y.m_type))
+
+			if y.m_linktask is not None:
+				linktask.set_run_after(y.m_linktask)
+
+			libpaths = libpaths + [y.path.bldpath(self.env)]
+
+
+		# add compiler flags
+		for i in self.to_list(self.uselib):
+			if self.env['DFLAGS_' + i]:
+				self.env.append_unique('DFLAGS', self.env['DFLAGS_' + i])
+		if self.dflags:
+			self.env.append_unique('DFLAGS', self.dflags)
+
+
+		# add import paths
+		for i in self.to_list(self.uselib):
+			if self.env['DPATH_' + i]:
+				importpaths += self.to_list(self.env['DPATH_' + i])
+		importpaths = self.to_list(self.importpaths) + importpaths
+
+		# now process the import paths
+		for path in importpaths:
+			if Utils.is_absolute_path(path):
+				imppath = path
+			else:
+				imppath = self.path.find_source_lst(Utils.split_path(path)).srcpath(self.env)
+			self.env.append_unique('_DIMPORTFLAGS', dpath_st % imppath)
+
+
+		# add library paths
+		for i in self.to_list(self.uselib):
+			if self.env['LIBPATH_' + i]:
+				libpaths += self.to_list(self.env['LIBPATH_' + i])
+
+		# now process the library paths
+		for path in libpaths:
+			self.env.append_unique('_DLIBDIRFLAGS', libpath_st % path)
+
+
+		# add libraries
+		for i in self.to_list(self.uselib):
+			if self.env['LIB_' + i]:
+				libs += self.to_list(self.env['LIB_' + i])
+
+		# now process the libraries
+		for lib in libs:
+			self.env.append_unique('_DLIBFLAGS', lib_st % lib)
+
+
+		# add linker flags
+		for i in self.to_list(self.uselib):
+			if self.env['DLINKFLAGS_' + i]:
+				self.env.append_unique('DLINKFLAGS', self.env['DLINKFLAGS_' + i])
+
+
+		# create compile tasks
+
+		compiletasks = []
+
+		obj_ext = self.env['D_' + type + '_obj_ext'][0]
+
+		find_source_lst = self.path.find_source_lst
+
+		for filename in self.to_list(self.source):
+			node = find_source_lst(Utils.split_path(filename))
+			base, ext = os.path.splitext(filename)
+
+			if not ext in self.s_default_ext:
+				fatal("unknown file " + filename)
+
+			task = self.create_task('d', self.env, 10)
+			task.set_inputs(node)
+			task.set_outputs(node.change_ext(obj_ext))
+
+			compiletasks.append(task)
+
+		# and after the objects, the remaining is the link step
+		# link in a lower priority (101) so it runs alone (default is 10)
+		global g_prio_link
+
+		outputs = []
+		for t in compiletasks: outputs.append(t.m_outputs[0])
+		linktask.set_inputs(outputs)
+		linktask.set_outputs(self.path.find_build(self.get_target_name()))
+
+		self.m_linktask = linktask
+
+
+	def get_target_name(self):
+		v = self.env
+
+		prefix = v['D_' + self.m_type + '_PREFIX']
+		suffix = v['D_' + self.m_type + '_SUFFIX']
+
+		if not prefix: prefix=''
+		if not suffix: suffix=''
+		return ''.join([prefix, self.target, suffix])
+
+
+	def install(self):
+		pass
+
+
+def setup(env):
+	d_str = '${D_COMPILER} ${DFLAGS} ${_DIMPORTFLAGS} ${D_SRC_F}${SRC} ${D_TGT_F}${TGT}'
+	link_str = '${D_LINKER} ${DLNK_SRC_F}${SRC} ${DLNK_TGT_F}${TGT} ${DLINKFLAGS} ${_DLIBDIRFLAGS} ${_DLIBFLAGS}'
+
+	Action.simple_action('d', d_str, 'GREEN')
+	Action.simple_action('d_link', link_str, color='YELLOW')
+
+	Object.register('d', dobj)
+
+
+def detect(conf):
+	return 1
+
---
+++ wafadmin/Tools/dmd.py
@@ -0,0 +1,88 @@
+#! /usr/bin/env python
+# encoding: utf-8
+# Carlos Rafael Giani, 2007 (dv)
+
+import sys
+
+
+def setup(env):
+	pass
+
+def detect(conf):
+	d_compiler = None
+	if conf.env['D_COMPILER']:
+		d_compiler = conf.env['D_COMPILER']
+	if not d_compiler: d_compiler = conf.find_program('dmd', var='D_COMPILER')
+	if not d_compiler:
+		return 0;
+
+	conf.check_tool('d', '.')
+
+	v = conf.env
+
+	#compiler
+	v['D_COMPILER']           = d_compiler
+
+	v['DFLAGS']               = []
+	v['_DIMPORTFLAGS']        = []
+
+	v['D_SRC_F']              = ''
+	v['D_TGT_F']              = '-c -of'
+	v['DPATH_ST']             = '-I%s' # template for adding import paths
+
+	# linker
+	v['D_LINKER']             = v['D_COMPILER']
+	v['DLNK_SRC_F']           = ''
+	v['DLNK_TGT_F']           = '-of'
+
+	v['DLIB_ST']              = '-L-l%s' # template for adding libs
+	v['DLIBPATH_ST']          = '-L-L%s' # template for adding libpaths
+	v['_DLIBDIRFLAGS']        = ''
+	v['_DLIBFLAGS']           = ''
+
+	# linker debug levels
+	v['DLINKFLAGS']            = ['-quiet']
+	v['DLINKFLAGS_OPTIMIZED']  = ['-O']
+	v['DLINKFLAGS_RELEASE']    = ['-O']
+	v['DLINKFLAGS_DEBUG']      = ['-g']
+	v['DLINKFLAGS_ULTRADEBUG'] = ['-g']
+
+
+	if sys.platform == "win32":
+		# shared library
+		v['D_shlib_obj_ext']       = ['.os']
+		v['D_shlib_PREFIX']        = 'lib'
+		v['D_shlib_SUFFIX']        = '.dll'
+
+		# static library
+		v['D_staticlib_obj_ext']   = ['.o']
+		v['D_staticlib_PREFIX']    = 'lib'
+		v['D_staticlib_SUFFIX']    = '.a'
+
+		# program
+		v['D_program_obj_ext']     = ['.o']
+		v['D_program_PREFIX']      = ''
+		v['D_program_SUFFIX']      = '.exe'
+
+	else:
+		# shared library
+		v['D_shlib_obj_ext']       = ['.os']
+		v['D_shlib_PREFIX']        = 'lib'
+		v['D_shlib_SUFFIX']        = '.so'
+
+		# static lib
+		v['D_staticlib_obj_ext']   = ['.o']
+		v['D_staticlib_PREFIX']    = 'lib'
+		v['D_staticlib_SUFFIX']    = '.a'
+
+		# program
+		v['D_program_obj_ext']     = ['.o']
+		v['D_program_PREFIX']      = ''
+		v['D_program_SUFFIX']      = ''
+
+
+	return 1
+
+
+def set_options(opt):
+	pass
