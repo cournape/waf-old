1d8fcd3790eecc5894277d7fbe62ebd7d26e7336
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2007-11-20 00:03:40 +0000

    Translation processing.
    git-svn-id: http://waf.googlecode.com/svn/trunk@1754 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/kde4.py
+++ wafadmin/Tools/kde4.py
@@ -2,7 +2,29 @@
 # encoding: utf-8
 # Thomas Nagy, 2006 (ita)
 
-import os, sys, re
+import os, sys, re, Object, Action, Utils, Common
+
+class langobj(Object.genobj):
+	s_default_ext = ['.java']
+	def __init__(self):
+		Object.genobj.__init__(self, 'other')
+		self.langs = ''
+		self.chmod = 0644
+		self.inst_dir = 'semantik'
+		self.inst_var = 'KDE4_LOCALE_INSTALL_DIR'
+
+	def apply(self):
+		for filename in self.to_list(self.langs):
+			node = self.path.find_source_lst(Utils.split_path(filename+'.po'))
+			task = self.create_task('msgfmt', self.env, 10)
+			task.set_inputs(node)
+			task.set_outputs(node.change_ext('.gmo'))
+
+	def install(self):
+		for i in self.m_tasks:
+			# only one output file for each task but well
+			lst=[a.relpath_gen(self.path) for a in i.m_outputs]
+			Common.install_files(self.inst_var, self.inst_dir, lst, chmod=self.chmod)
 
 def detect(conf):
 	kdeconfig = conf.find_program('kde4-config')
@@ -35,8 +57,16 @@ def detect(conf):
 	conf.env['LIB_KDECORE']='kdecore'
 	conf.env['LIB_KDEUI']  ='kdeui'
 	conf.env['LIB_KIO']    ='kio'
+	conf.env['LIB_KHTML']  ='khtml'
+	conf.env['LIB_KPARTS'] ='kparts'
 
 	conf.env['LIBPATH_KDECORE'] = conf.env['KDE4_LIB_INSTALL_DIR']
 	conf.env['CPPPATH_KDECORE'] = conf.env['KDE4_INCLUDE_INSTALL_DIR']
+	conf.env.append_value('CPPPATH_KDECORE', conf.env['KDE4_INCLUDE_INSTALL_DIR']+"/KDE")
 
+	conf.env['MSGFMT'] = conf.find_program('msgfmt')
+
+def setup(env):
+	Object.register('msgfmt', langobj)
+	Action.simple_action('msgfmt', '${MSGFMT} ${SRC} -o ${TGT}', color='BLUE')
 
