fdd8d294769ebd4cfa0795d8e0fbb240823eb179
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-01-04 17:27:16 +0000

    Gob2 tool by Ali Sabil
    git-svn-id: http://waf.googlecode.com/svn/trunk@1943 f0382ac9-c320-0410-b3f0-b508d59f5a85

---
+++ wafadmin/Tools/gob2.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# encoding: utf-8
+# Ali Sabil, 2007
+
+import Action, Object
+
+gob2_str = '${GOB2} -o ${TGT[0].bld_dir(env)} ${GOB2FLAGS} ${SRC}'
+
+def gob2_file(self, node):
+	gob2task = self.create_task('gob2', nice=40)
+	gob2task.set_inputs(node)
+	gob2task.set_outputs(node.change_ext('.c'))
+
+	task = self.create_task('cc')
+	task.set_inputs(gob2task.m_outputs)
+	task.set_outputs(node.change_ext('.o'))
+
+def setup(bld):
+	# create our action here
+	Action.simple_action('gob2', gob2_str, color='BLUE')
+
+	# register the hook for use with ccobj
+	try: Object.hook('cc', 'GOB2_EXT', gob2_file)
+	except: pass
+
+def detect(conf):
+	gob2 = conf.find_program('gob2', var='GOB2')
+	if not gob2: return
+	conf.env['GOB2'] = gob2
+	conf.env['GOB2FLAGS'] = ''
+	conf.env['GOB2_EXT'] = ['.gob']
+
