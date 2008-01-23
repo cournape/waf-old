b08ac828f4c5773ec01c76bf096428a1ff9667c5
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-01-23 18:59:24 +0000

    Missing copyright notice.
    git-svn-id: http://waf.googlecode.com/svn/trunk@2141 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Constants.py
+++ wafadmin/Constants.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 # encoding: utf-8
+# Yinon dot me gmail 2008
 
 "Waf constants"
 
@@ -9,7 +10,7 @@ CACHE_DIR			=	'_cache_'
 CACHE_SUFFIX		=	'.cache.py'
 WSCRIPT_FILE		=	'wscript'
 WSCRIPT_BUILD_FILE	=	'wscript_build'
-		
+
 SRCDIR				=	'srcdir'
 BLDDIR				=	'blddir'
 APPNAME				=	'APPNAME'
