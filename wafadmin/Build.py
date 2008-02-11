5d2415cd2d326f817d58159a559951db7ff62e8a
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-02-11 00:01:18 +0000

    Typo.
    git-svn-id: http://waf.googlecode.com/svn/trunk@2401 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Build.py
+++ wafadmin/Build.py
@@ -402,7 +402,7 @@ class Build(object):
 		#debug("rescanning "+str(src_dir_node), 'build')
 
 		# TODO undocumented hook
-		if self.hasattr('repository'): self.repository()
+		if hasattr(self, 'repository'): self.repository()
 
 		# list the files in the src directory, adding the signatures
 		files = self.scan_src_path(src_dir_node, src_dir_node.abspath(), src_dir_node.files())
