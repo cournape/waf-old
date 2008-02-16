b6fda09187a4b231e15787c435f5bfc6b4792c4f
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-02-16 20:26:36 +0000

    This should close #93
    git-svn-id: http://waf.googlecode.com/svn/trunk@2447 f0382ac9-c320-0410-b3f0-b508d59f5a85

---
+++ demos/gnome/mkenums/prod.h
@@ -0,0 +1,18 @@
+
+/* Generated data (by glib-mkenums) */
+
+#ifndef __BLAH_H__
+#define __BLAH_H__
+#include <glib-object.h>
+G_BEGIN_DECLS
+/* enumerations from "a.h" */
+GType foo_foo_get_type(void) G_GNUC_CONST;
+#define AWN_TYPE_FOO (foo_foo_get_type())
+/* enumerations from "b.h" */
+GType foo2_foo2_get_type(void) G_GNUC_CONST;
+#define AWN_TYPE_FOO2 (foo2_foo2_get_type())
+G_END_DECLS
+#endif
+
+/* Generated data ends here */
+
