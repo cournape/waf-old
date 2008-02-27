19c20a33c88caca40bd6ea012332ab2487cea516
yinon.me <yinon.me@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-02-27 12:55:54 +0000

    Use the new testing module.
    git-svn-id: http://waf.googlecode.com/svn/trunk@2622 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Test.py
+++ wafadmin/Test.py
@@ -21,23 +21,25 @@ def testname(file, tests_dir='test'):
 	return open(test_file, 'r')
 
 def run_tests():
-	try:
-		# when running from wafadmin dir
-		import test.build_dir as test_build_dir
-		import test.gpp_test as test_gpp
-		import test.gcc_test as test_gcc
-	except ImportError:
-		# when running from waf dir
-		import wafadmin.test.build_dir as test_build_dir
-		import wafadmin.test.gpp_test as test_gpp
-		import wafadmin.test.gcc_test as test_gcc
+	# could be run from main waf dir only !
+	import wafadmin.test.build_dir as test_build_dir
+	import wafadmin.test.gpp_test as test_gpp
+	import wafadmin.test.gcc_test as test_gcc
+	import wafadmin.test.configure_test as test_configure
 
+	if Params.g_options:
+		verbose = Params.g_options.verbose
+	else:
+		verbose = 1
+
+	info("******** Configure tests ********")
+	test_configure.run_tests(verbose)
 	info("******** build dir tests ********")
-	test_build_dir.run_tests(Params.g_options.verbose)
+	test_build_dir.run_tests(verbose)
 	info("******** g++ tests ********")
-	test_gpp.run_tests(Params.g_options.verbose)
+	test_gpp.run_tests(verbose)
 	info("******** gcc tests ********")
-	test_gcc.run_tests(Params.g_options.verbose)
+	test_gcc.run_tests(verbose)
 
 	for i in ['dist','configure','clean','distclean','make','install','doc']:
 		Params.g_commands[i]=0
