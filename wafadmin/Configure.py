cb48a7a7ebcf5120c1d75d4ce9966f8805eeced1
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-03-01 20:18:41 +0000

    Simplification.
    git-svn-id: http://waf.googlecode.com/svn/trunk@2670 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Configure.py
+++ wafadmin/Configure.py
@@ -201,6 +201,12 @@ class Configure(object):
 		self.env = self.retrieve(name)
 		self.envname = name
 
+	def add_os_flags(self, var, dest=None):
+		if not dest: dest = var
+		# do not use 'get' to make certain the variable is not defined
+		try: self.env[dest] = os.environ[var]
+		except KeyError: pass
+
 	def find_program(self, program_name, path_list=[], var=None):
 		"wrapper provided for convenience"
 		ret = find_program_impl(self.env, program_name, path_list, var)
