5db54460219153bb01736a751441928d8b78e4e1
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-03-07 20:11:53 +0000

    Additional conditions to prevent installation: * env[VAR] is not defined * if a dict is provided, dict['var'] is set to 0
    git-svn-id: http://waf.googlecode.com/svn/trunk@2707 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Task.py
+++ wafadmin/Task.py
@@ -61,9 +61,11 @@ class TaskManager(object):
 			if type(d) is types.FunctionType:
 				d(tsk)
 			elif type(d) is types.StringType:
+				if not tsk.env()[d]: return
 				lst = [a.relpath_gen(Params.g_build.m_srcnode) for a in tsk.m_outputs]
 				Common.install_files(tsk.env()[d], '', lst, chmod=0644, env=tsk.env())
 			else:
+				if d['var'] == 0: return
 				lst = [a.relpath_gen(Params.g_build.m_srcnode) for a in tsk.m_outputs]
 				if d.get('src', 0): lst += [a.relpath_gen(Params.g_build.m_srcnode) for a in tsk.m_inputs]
 				Common.install_files(d['var'], d['dir'], lst, chmod=d.get('chmod', 0644), env=tsk.env())
