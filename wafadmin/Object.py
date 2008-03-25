d4cf5990426d91db41e1d547a4596f88d9098214
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-03-25 20:44:12 +0000

    Typo
    git-svn-id: http://waf.googlecode.com/svn/trunk@2893 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Object.py
+++ wafadmin/Object.py
@@ -99,8 +99,8 @@ class register_obj(type):
 	"""
 	def __init__(cls, name, bases, dict):
 		super(register_obj, cls).__init__(name, bases, dict)
-		if cls.__name__.endswith('taskgen'):
-			task_gen.classes[cls.__name__[:-3]] = cls
+		if cls.__name__.endswith('_taskgen'):
+			task_gen.classes[cls.__name__[:-8]] = cls
 
 class task_gen(object):
 	"""
