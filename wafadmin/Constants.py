ec48742dba72234f3b6650416747da8195eb3f05
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-03-30 13:09:48 +0000

    The data structures have changed somewhat.
    git-svn-id: http://waf.googlecode.com/svn/trunk@2938 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Constants.py
+++ wafadmin/Constants.py
@@ -4,7 +4,7 @@
 
 # maintainer the version number is updated from the top-level wscript file
 HEXVERSION = 0x10303
-ABI = 3
+ABI = 4
 
 CACHE_DIR          = 'c4che'
 CACHE_SUFFIX       = '.cache.py'
