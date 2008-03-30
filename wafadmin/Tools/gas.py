232271559b052d5ce5844be0322e218a239a4273
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-03-30 22:09:54 +0000

    Fixes issue #123
    git-svn-id: http://waf.googlecode.com/svn/trunk@2949 f0382ac9-c320-0410-b3f0-b508d59f5a85

---
+++ wafadmin/Tools/gas.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# encoding: utf-8
+# Thomas Nagy, 2008 (ita)
+
+"as and gas"
+
+import os, sys
+import Action
+from Object import extension, taskgen, after
+
+EXT_ASM = ['.s', '.S', '.asm', '.ASM', '.spp', '.SPP']
+
+as_str = '${AS} ${ASFLAGS} ${_ASINCFLAGS} ${SRC} -o ${TGT}'
+Action.simple_action('asm', as_str, 'PINK', prio=100)
+
+@extension(EXT_ASM)
+def asm_hook(self, node):
+	# create the compilation task: cpp or cc
+	task = self.create_task('asm', self.env)
+	try: obj_ext = self.obj_ext
+	except AttributeError: obj_ext = '_%s.o' % self.m_type[:2]
+
+	task.m_inputs = [node]
+	task.m_outputs = [node.change_ext(obj_ext)]
+	self.compiled_tasks.append(task)
+
+@taskgen
+@after('apply_obj_vars_cc')
+@after('apply_obj_vars_cxx')
+def asm_incflags(self):
+	if self.env['ASINCFLAGS']: self.env['_ASINCFLAGS'] = self.env['ASINCFLAGS']
+	if 'cxx' in self.features: self.env['_ASINCFLAGS'] = self.env['_CCINCFLAGS']
+	else: self.env['_ASINCFLAGS'] = self.env['_CXXINCFLAGS']
+
+def detect(conf):
+	comp = os.environ.get('AS', '')
+	if not comp = conf.find_program('as', var='AS')
+	if not comp: comp = conf.find_program('gas', var='AS')
+	if not comp: comp = conf.env['CC']
+	if not comp: return
+
+	v = conf.env
+	v['ASFLAGS']  = ''
+
