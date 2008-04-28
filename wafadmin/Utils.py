58432838912f5779fe52483743d2c2107cec29eb
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-04-28 23:02:57 +0000

    Better to use int as input
    git-svn-id: http://waf.googlecode.com/svn/trunk@3285 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Utils.py
+++ wafadmin/Utils.py
@@ -63,7 +63,7 @@ if sys.platform == "win32":
 def to_int(s):
 	lst = s.split(".")
 
-def waf_version(mini = "0.0.1", maxi = "100.0.0"):
+def waf_version(mini = 0x010000, maxi = 0x100000):
 	"throws an exception if the waf version is wrong"
 	ver = HEXVERSION
 	try: min_val = mini + 0
