e633233c10013a789afe90e3b7eece14169e91a7
yinon.me <yinon.me@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-05-21 08:42:13 +0000

    continue the refactoring...
    git-svn-id: http://waf.googlecode.com/svn/trunk@3404 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- demos/adv/dang.py
+++ demos/adv/dang.py
@@ -4,9 +4,9 @@
 
 "Demo: '.coin' files are converted into cpp files using 'cat': {.coin -> .cpp -> .o}"
 
-import Object
+import TaskGen
 
-Object.declare_chain(
+TaskGen.declare_chain(
 	name = 'dang',
 	action = '${DANG} ${SRC} > ${TGT}',
 	ext_in = '.coin',
