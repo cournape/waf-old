18a452fc0426bb7bbe63a649d1f61478656c063c
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-06-01 19:24:04 +0000

    Debugging helper
    git-svn-id: http://waf.googlecode.com/svn/trunk@3483 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Playground/Parallel.py
+++ wafadmin/Playground/Parallel.py
@@ -2,10 +2,10 @@
 # encoding: utf-8
 # Thomas Nagy, 2007 (ita)
 
-# debugging helpers for parallel compilation
-dostat=0
 """
-output a stat file (data for gnuplot) when running tasks in parallel
+debugging helpers for parallel compilation
+
+To output a stat file (data for gnuplot) when running tasks in parallel:
 
 #! /usr/bin/gnuplot -persist
 set terminal png
@@ -14,43 +14,95 @@ set yrange [-1:6]
 plot 'test.dat' using 1:3 with linespoints
 """
 
-stat = []
+import time, threading
+import Runner
+from Constants import *
+
+INTERVAL = 0.01
+
+
+mylock = threading.Lock()
+state = 0
+def set_running(by):
+	mylock.acquire()
+	global state
+	state += by
+	mylock.release()
+
+def newrun(self):
+	m = self.master
+
+	while 1:
+		tsk = m.ready.get()
+		if m.failed and not m.running:
+			m.out.put(tsk)
+			continue
+
+		set_running(1)
+		Runner.printout(tsk.get_display())
+		ret = tsk.run()
+		set_running(-1)
+
+		if ret:
+			tsk.err_code = ret
+			tsk.m_hasrun = CRASHED
+		else:
+			try:
+				tsk.update_stat()
+			except OSError:
+				tsk.m_hasrun = MISSING
+			else:
+				tsk.m_hasrun = SUCCESS
+		if tsk.m_hasrun != SUCCESS: # TODO for now, do no keep running in parallel  and not Params.g_options.keep:
+			m.failed = 1
+
+		m.out.put(tsk)
+		#set_running(-1)
+
+Runner.TaskConsumer.run = newrun
+
 class TaskPrinter(threading.Thread):
-	def __init__(self, id, master):
+	def __init__(self, master):
 		threading.Thread.__init__(self)
 		self.setDaemon(1)
 		self.m_master = master
+		self.stat = []
 		self.start()
 
 	def run(self):
-		global count, lock, running, stat
+		global state
+		while self.m_master:
+			try:
+				#self.stat.append( (time.time(), self.m_master.progress, state) )
+				self.stat.append( (time.time(), self.m_master.progress, self.m_master.ready.qsize()) )
+			except:
+				pass
+
+			try: time.sleep(INTERVAL)
+			except: pass
+
 		while 1:
-			lock.acquire()
-			stat.append( (time.time(), count, running) )
-			lock.release()
-			time.sleep(0.1)
+			try:
+				time.sleep(60)
+			except:
+				pass
 
-	def do_stat(self, num):
-		global running
-		lock.acquire()
-		running += num
-		lock.release()
 
-	def start(self):
-		if dostat: TaskPrinter(-1, self)
+old_start = Runner.Parallel.start
+def do_start(self):
+	collector = TaskPrinter(self)
+	old_start(self)
+	collector.m_master = None
 
-		# Runner.py code here
+	if len(collector.stat) <= 0:
+		print "nothing to display! start from an empty build"
+	else:
+		file = open('/tmp/test.dat', 'w')
+		(t1, queue, run) = collector.stat[0]
+		for (time, queue, run) in collector.stat:
+			#print time, t1, queue, run
+			file.write("%f %f %f\n" % (time-t1, queue, run))
+		file.close()
+Runner.Parallel.start = do_start
 
-		# in parallel mode, what matters is the amount of misses: trying to launch tasks that cannot be run yet
-		# there are two schemes to reduce that latency
-		# * separate the tasks in independant groups that can be parallelized (often the case)
-		# * use a heuristic to take the next task from the list of awaiting tasks
-		debug("amount of loops "+str(loop), 'runner')
-		global stat
-		if dostat and stat:
-			file = open('test.dat', 'w')
-			(t1, queue, run) = stat[0]
-			for (time, queue, run) in stat:
-				file.write("%f %f %f\n" % (time-t1, queue, run))
-			file.close()
 
