d5d165c292eed1932e473f62dd4da7ba2742739f
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-06-06 20:30:07 +0000

    Code cleanup.
    git-svn-id: http://waf.googlecode.com/svn/trunk@3558 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- utils/amview.py
+++ utils/amview.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python
 
-## 
-# @file 
+##
+# @file
 # waf Makefile.am related tools
-# 
-# note: may be a future base of a Makefile.am to scons converter if someone like to write it 
+#
+# note: may be a future base of a Makefile.am to scons converter if someone like to write it
 #
 # (@) 2005 Ralf Habacker  - published under the GPL license
 #
- 
+
 import amtool
 
 amtool.amview()
