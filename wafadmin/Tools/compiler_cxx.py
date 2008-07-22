f0eae3c56f300fad127996d275e10d5640b12941
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-07-22 17:33:22 +0000

    Code cleanup.
    git-svn-id: http://waf.googlecode.com/svn/trunk@4222 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/compiler_cxx.py
+++ wafadmin/Tools/compiler_cxx.py
@@ -41,23 +41,18 @@ def set_options(opt):
 	possible_compiler_list = __list_possible_compiler(detected_platform)
 	test_for_compiler = str(" ").join(possible_compiler_list)
 	cxx_compiler_opts = opt.add_option_group("C++ Compiler Options")
-	try:
-		cxx_compiler_opts.add_option('--check-cxx-compiler', default="%s" % test_for_compiler,
-			help='On this platform (%s) the following C++ Compiler will be checked by default: "%s"' %
-								(detected_platform, test_for_compiler),
-			dest="check_cxx_compiler")
-	except optparse.OptionConflictError:
-		pass
+	cxx_compiler_opts.add_option('--check-cxx-compiler', default="%s" % test_for_compiler,
+		help='On this platform (%s) the following C++ Compiler will be checked by default: "%s"' %
+			(detected_platform, test_for_compiler),
+		dest="check_cxx_compiler")
+
 	for cxx_compiler in test_for_compiler.split():
 		opt.tool_options('%s' % cxx_compiler, option_group=cxx_compiler_opts)
 
-	try:
-		opt.add_option('-d', '--debug-level',
+	opt.add_option('-d', '--debug-level',
 		action = 'store',
 		default = ccroot.DEBUG_LEVELS.RELEASE,
 		help = "Specify the debug level, does nothing if CXXFLAGS is set in the environment. [Allowed Values: '%s']" % "', '".join(ccroot.DEBUG_LEVELS.ALL),
 		choices = ccroot.DEBUG_LEVELS.ALL,
 		dest = 'debug_level')
-	except optparse.OptionConflictError:
-		pass
 
