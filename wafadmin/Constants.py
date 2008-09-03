6c35b1ab7ff1c0ce48a9e6dec65bd5d236ac5799
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-09-03 17:58:00 +0000

    version number is 1.5.0 now
    git-svn-id: http://waf.googlecode.com/svn/trunk@4430 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Constants.py
+++ wafadmin/Constants.py
@@ -8,8 +8,8 @@ these constants are somewhat public, try not to mess them
 maintainer: the version number is updated from the top-level wscript file
 """
 
-HEXVERSION = 0x10409
-WAFVERSION = "1.4.9"
+HEXVERSION = 0x10500
+WAFVERSION = "1.5.0"
 ABI = 6
 
 CACHE_DIR          = 'c4che'
