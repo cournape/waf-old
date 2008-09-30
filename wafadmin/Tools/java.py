0409f874877199c9e05062dde895179bd1083534
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-09-30 10:56:45 +0000

    Update.
    git-svn-id: http://waf.googlecode.com/svn/trunk@4608 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/java.py
+++ wafadmin/Tools/java.py
@@ -23,71 +23,67 @@ change is only annoying for the compilation times
 import os, re
 from Configure import conf
 import TaskGen, Task, Utils
+from TaskGen import feature, taskgen
 
 class java_taskgen(TaskGen.task_gen):
-	def __init__(self, *k):
-		TaskGen.task_gen.__init__(self, *k)
+	def __init__(self, *k, **kw):
+		TaskGen.task_gen.__init__(self, *k, **kw)
 
-		self.jarname = ''
-		self.jaropts = ''
-		self.classpath = ''
-		self.source_root = '.'
+@taskgen
+@feature('java')
+def apply_java(self):
+	Utils.def_attrs(self, jarname='', jaropts='', classpath='',
+		source_root='.', jar_mf_attributes={}, jar_mf_classpath=[])
 
-		# Jar manifest attributes
-		# TODO: Add manifest creation
-		self.jar_mf_attributes = {}
-		self.jar_mf_classpath = []
+	nodes_lst = []
 
-	def apply(self):
-		nodes_lst = []
+	if not self.classpath:
+		if not self.env['CLASSPATH']:
+			self.env['CLASSPATH'] = '..' + os.pathsep + '.'
+	else:
+		self.env['CLASSPATH'] = self.classpath
 
-		if not self.classpath:
-			if not self.env['CLASSPATH']:
-				self.env['CLASSPATH'] = '..' + os.pathsep + '.'
-		else:
-			self.env['CLASSPATH'] = self.classpath
+	re_foo = re.compile(self.source)
 
-		re_foo = re.compile(self.source)
+	source_root_node = self.path.find_dir(self.source_root)
 
-		source_root_node = self.path.find_dir(self.source_root)
+	src_nodes = []
+	bld_nodes = []
 
-		src_nodes = []
-		bld_nodes = []
+	prefix_path = source_root_node.abspath()
+	for (root, dirs, filenames) in os.walk(source_root_node.abspath()):
+		for x in filenames:
+			file = root + '/' + x
+			file = file.replace(prefix_path, '')
+			if file.startswith('/'):
+				file = file[1:]
 
-		prefix_path = source_root_node.abspath()
-		for (root, dirs, filenames) in os.walk(source_root_node.abspath()):
-			for x in filenames:
-				file = root + '/' + x
-				file = file.replace(prefix_path, '')
-				if file.startswith('/'):
-					file = file[1:]
+			if re_foo.search(file) > -1:
+				node = source_root_node.find_resource(file)
+				src_nodes.append(node)
 
-				if re_foo.search(file) > -1:
-					node = source_root_node.find_resource(file)
-					src_nodes.append(node)
+				node2 = node.change_ext(".class")
+				bld_nodes.append(node2)
 
-					node2 = node.change_ext(".class")
-					bld_nodes.append(node2)
+	self.env['OUTDIR'] = source_root_node.abspath(self.env)
 
-		self.env['OUTDIR'] = source_root_node.abspath(self.env)
+	tsk = self.create_task('javac')
+	tsk.set_inputs(src_nodes)
+	tsk.set_outputs(bld_nodes)
 
-		tsk = self.create_task('javac')
-		tsk.set_inputs(src_nodes)
-		tsk.set_outputs(bld_nodes)
+	if self.jarname:
+		tsk = self.create_task('jar_create')
+		tsk.set_inputs(bld_nodes)
+		tsk.set_outputs(self.path.find_or_declare(self.jarname))
 
-		if self.jarname:
-			tsk = self.create_task('jar_create')
-			tsk.set_inputs(bld_nodes)
-			tsk.set_outputs(self.path.find_or_declare(self.jarname))
+		if not self.env['JAROPTS']:
+			if self.jaropts:
+				self.env['JAROPTS'] = self.jaropts
+			else:
+				dirs = '.'
+				self.env['JAROPTS'] = '-C %s %s' % (self.env['OUTDIR'], dirs)
 
-			if not self.env['JAROPTS']:
-				if self.jaropts:
-					self.env['JAROPTS'] = self.jaropts
-				else:
-					dirs = '.'
-					self.env['JAROPTS'] = '-C %s %s' % (self.env['OUTDIR'], dirs)
-
-Task.simple_task_type('javac', '${JAVAC} -classpath ${CLASSPATH} -d ${OUTDIR} ${SRC}', color='BLUE', before="jar_create")
+Task.simple_task_type('javac', '${JAVAC} -classpath ${CLASSPATH} -d ${OUTDIR} ${SRC}', color='BLUE', before='jar_create')
 Task.simple_task_type('jar_create', '${JAR} ${JARCREATE} ${TGT} ${JAROPTS}', color='GREEN')
 
 def detect(conf):
@@ -113,9 +109,7 @@ def detect(conf):
 
 @conf
 def check_java_class(conf, classname, with_classpath=None):
-	"""
-	Check if specified java class is installed.
-	"""
+	"""Check if the specified java class is installed"""
 
 	class_check_source = """
 public class Test {
