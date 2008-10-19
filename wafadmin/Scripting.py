34d027b8d57642f53f0cb598acbfc0d60aa96365
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-10-19 12:54:57 +0000

    Exclude .gitignore and .cvsignore
    git-svn-id: http://waf.googlecode.com/svn/trunk@4725 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Scripting.py
+++ wafadmin/Scripting.py
@@ -297,7 +297,7 @@ def main():
 	fun = getattr(Utils.g_module, 'shutdown', None)
 	if fun: fun()
 
-excludes = '.bzr .bzrignore .git .svn CVS .arch-ids {arch} SCCS BitKeeper .hg Makefile Makefile.in config.log'.split()
+excludes = '.bzr .bzrignore .git .gitignore .svn CVS .cvsignore .arch-ids {arch} SCCS BitKeeper .hg Makefile Makefile.in config.log'.split()
 dist_exts = '~ .rej .orig .pyc .pyo .bak .tar.bz2 tar.gz .zip'.split()
 def dont_dist(name, src, build_dir):
 	global excludes, dist_exts
