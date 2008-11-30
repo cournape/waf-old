3735b3317d5ec29c4f74a6d761e3ed69f7e9a4fa
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-11-30 19:29:28 +0000

    Make it look more tidy
    git-svn-id: http://waf.googlecode.com/svn/trunk@5003 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- playground/doxy/doxygen.py
+++ playground/doxy/doxygen.py
@@ -16,15 +16,6 @@ DOXY_EXTS = """
 .idl .odl .php .php3 .inc .m .mm
 """.split()
 
-def runnable_status(self):
-	if not getattr(self, 'pars', None):
-		infile = self.inputs[0].abspath(self.env)
-		self.pars = read_into_dict(infile)
-		if not self.pars.get('OUTPUT_DIRECTORY', None):
-			self.pars['OUTPUT_DIRECTORY'] = self.inputs[0].parent.abspath(self.env)
-	self.signature()
-	return Task.Task.runnable_status(self)
-
 def filter_match(node_list):
 	buf = []
 	for x in node_list:
@@ -33,50 +24,6 @@ def filter_match(node_list):
 			if name.endswith(y):
 				buf.append(x)
 	return buf
-
-def doxy_scan(self):
-
-	recurse = self.pars.get('RECURSIVE', None) == 'YES'
-
-	def nodes_files_of(node):
-		# perform the listdir in the source directory, once
-		node.__class__.bld.rescan(node)
-
-		# doxygen looks at the files under the source directory
-		buf = []
-		for x in node.__class__.bld.cache_dir_contents[node.id]:
-			filename = node.abspath() + os.sep + x
-			st = os.stat(filename)
-			if stat.S_ISREG(st[stat.ST_MODE]):
-				k = node.find_resource(x)
-				buf.append(node.find_resource(x))
-			elif stat.S_ISDIR(st[stat.ST_MODE]):
-				if recurse:
-					nd = node.find_dir(x)
-					if nd.id != nd.__class__.bld.bldnode.id:
-						buf += nodes_files_of(nd)
-		return buf
-
-	ret = nodes_files_of(self.inputs[0].parent)
-	ret = filter_match(ret)
-
-	return (ret, [])
-
-def doxy_run(self):
-	code = '\n'.join(['%s = %s' % (x, self.pars[x]) for x in self.pars])
-	if not self.env['DOXYFLAGS']:
-		self.env['DOXYFLAGS'] = ''
-	cmd = Utils.subst_vars(DOXY_STR % (self.inputs[0].parent.abspath()), self.env)
-	proc = pproc.Popen(cmd, shell=True, stdin=pproc.PIPE)
-	proc.communicate(code)
-	return proc.returncode
-
-cls = Task.task_type_from_func('doxygen', func=doxy_run, vars=['DOXYGEN', 'DOXYFLAGS'])
-cls.runnable_status = runnable_status
-cls.scan = doxy_scan
-cls.color = 'BLUE'
-cls.after = 'cxx_link cc_link'
-cls.quiet = True
 
 re_join = re.compile(r'\\(\r)*\n', re.M)
 re_nl = re.compile('\r*\n', re.M)
@@ -100,6 +47,59 @@ def read_into_dict(name):
 		if len(tmp) < 2: continue
 		ret[tmp[0].strip()] = '='.join(tmp[1:]).strip()
 	return ret
+
+class doxygen_task(Task.Task):
+
+	vars=['DOXYGEN', 'DOXYFLAGS']
+	color = 'BLUE'
+	after = 'cxx_link cc_link'
+	quiet = True
+
+	def runnable_status(self):
+		if not getattr(self, 'pars', None):
+			infile = self.inputs[0].abspath(self.env)
+			self.pars = read_into_dict(infile)
+			if not self.pars.get('OUTPUT_DIRECTORY', None):
+				self.pars['OUTPUT_DIRECTORY'] = self.inputs[0].parent.abspath(self.env)
+		self.signature()
+		return Task.Task.runnable_status(self)
+
+	def scan(self):
+
+		recurse = self.pars.get('RECURSIVE', None) == 'YES'
+
+		def nodes_files_of(node):
+			# perform the listdir in the source directory, once
+			node.__class__.bld.rescan(node)
+
+			# doxygen looks at the files under the source directory
+			buf = []
+			for x in node.__class__.bld.cache_dir_contents[node.id]:
+				filename = node.abspath() + os.sep + x
+				st = os.stat(filename)
+				if stat.S_ISREG(st[stat.ST_MODE]):
+					k = node.find_resource(x)
+					buf.append(node.find_resource(x))
+				elif stat.S_ISDIR(st[stat.ST_MODE]):
+					if recurse:
+						nd = node.find_dir(x)
+						if nd.id != nd.__class__.bld.bldnode.id:
+							buf += nodes_files_of(nd)
+			return buf
+
+		ret = nodes_files_of(self.inputs[0].parent)
+		ret = filter_match(ret)
+
+		return (ret, [])
+
+	def run(self):
+		code = '\n'.join(['%s = %s' % (x, self.pars[x]) for x in self.pars])
+		if not self.env['DOXYFLAGS']:
+			self.env['DOXYFLAGS'] = ''
+		cmd = Utils.subst_vars(DOXY_STR % (self.inputs[0].parent.abspath()), self.env)
+		proc = pproc.Popen(cmd, shell=True, stdin=pproc.PIPE)
+		proc.communicate(code)
+		return proc.returncode
 
 @feature('doxygen')
 def process_doxy(self):
