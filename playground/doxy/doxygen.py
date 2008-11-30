11a5413b091347e9e208e46a92fb9350f61145f1
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-11-30 13:25:00 +0000

    read the doxyfile from stdin
    git-svn-id: http://waf.googlecode.com/svn/trunk@4984 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- playground/doxy/doxygen.py
+++ playground/doxy/doxygen.py
@@ -2,12 +2,69 @@
 # encoding: UTF-8
 # Thomas Nagy 2008
 
+import re
+import pproc
 import Task, Utils
 from TaskGen import feature
 
-doxy_str = 'cd ${SRC[0].parent.abspath()} && ${DOXYGEN} ${DOXYFLAGS} ${SRC[0].abspath(env)}'
-cls = Task.simple_task_type('doxygen', doxy_str, color='BLUE', after='cxx_link cc_link')
+DOXY_EXTS = """
+.c .cc .cxx .cpp .c++ .C
+.h .hh .hxx .hpp .h++ .H
+.py .java .cs
+.ii .ixx .ipp .i++ .inl
+.idl .odl .php .php3 .inc .m .mm
+""".split()
+
+
+def doxy_run(self):
+	infile = self.inputs[0].abspath(self.env)
+
+	vars = read_into_dict(infile)
+
+	if not vars.get('OUTPUT_DIRECTORY', None):
+		vars['OUTPUT_DIRECTORY'] = self.inputs[0].parent.abspath(self.env)
+
+	code = '\n'.join(['%s = %s' % (x, vars[x]) for x in vars])
+
+
+	self.env['DOXYFLAGS'] = ''
+	cmd = Utils.subst_vars('cd %s && ${DOXYGEN} ${DOXYFLAGS} -' % (self.inputs[0].parent.abspath()), self.env)
+	proc = pproc.Popen(cmd, shell=True, stdin=pproc.PIPE)
+	ret = proc.communicate(code)
+
+	print ret
+	return 0
+
+
+#doxy_str = 'cd ${SRC[0].parent.abspath()} && ${DOXYGEN} ${DOXYFLAGS} ${SRC[0].abspath(env)}'
+#cls = Task.simple_task_type('doxygen', doxy_str, color='BLUE', after='cxx_link cc_link')
+cls = Task.task_type_from_func('doxygen', func=doxy_run, vars=['DOXYGEN', 'DOXYFLAGS'])
+cls.color = 'BLUE'
+cls.after = 'cxx_link cc_link'
 cls.quiet = True
+
+re_join = re.compile(r'\\(\r)*\n', re.M)
+re_nl = re.compile('\r*\n', re.M)
+
+def read_into_dict(name):
+	f = open(name, 'rb')
+	txt = f.read()
+	f.close()
+
+	ret = {}
+
+	txt = re_join.sub('', txt)
+	lines = re_nl.split(txt)
+	vals = []
+
+	for x in lines:
+		x.strip()
+		if len(x) < 2: continue
+		if x[0] == '#': continue
+		tmp = x.split('=')
+		if len(tmp) < 2: continue
+		ret[tmp[0].strip()] = '='.join(tmp[1:]).strip()
+	return ret
 
 @feature('doxygen')
 def process_doxy(self):
@@ -15,6 +72,7 @@ def process_doxy(self):
 		return
 
 	node = self.path.find_resource(self.doxyfile)
+	if not node: raise ValueError, 'doxygen file not found'
 
 	# the task instance
 	tsk = self.create_task('doxygen')
