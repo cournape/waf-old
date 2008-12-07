95817b2c39108610b6c96d04add666ba08ca5af7
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-12-07 23:14:12 +0000

    Try to solve issue #176 without excessive code duplication
    git-svn-id: http://waf.googlecode.com/svn/trunk@5067 f0382ac9-c320-0410-b3f0-b508d59f5a85

---
+++ playground/objc/gcc-objc.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# encoding: utf-8
+# Samuel Mendes, 2008 (lok)
+
+import cc
+from TaskGen import extension
+from Configure import conftest
+
+# and for objective c++ it would be .mm and .M
+
+EXT_OBJC = ['.m']
+@extension(EXT_OBJC)
+def objc_hook(self, node):
+	tsk = cc.c_hook(self, node)
+	tsk.env.append_unique('CCFLAGS', tsk.env['GCC-OBJC'])
+	tsk.env.append_unique('LINKFLAGS', tsk.env['GCC-OBJC'])
+	print tsk.env
+
+@conftest
+def gccobjc_common_flags(conf):
+	v = conf.env
+	v['GCC-OBJC']         = '-x objective-c'
+	v['GCC-OBJCLINK']     = '-lobjc'
+	#v['FRAMEWORKPATH_ST'] = '-F%s'
+
+detect = 'gccobjc_common_flags'
