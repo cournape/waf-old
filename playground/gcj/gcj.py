ce3b96cf2c988c063b518ac0bf31104da78cadd8
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-12-19 00:12:17 +0000

    Working demo, native compilation with gcj
    git-svn-id: http://waf.googlecode.com/svn/trunk@5147 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- playground/gcj/gcj.py
+++ playground/gcj/gcj.py
@@ -5,17 +5,18 @@
 import os, re
 from Configure import conf
 import TaskGen, Task, Utils
-from TaskGen import feature, taskgen
+from TaskGen import feature, before
 
-class java_taskgen(TaskGen.task_gen):
-	def __init__(self, *k, **kw):
-		TaskGen.task_gen.__init__(self, *k, **kw)
-
-@taskgen
-@feature('java')
-def apply_java(self):
+@feature('gcj')
+@before('apply_java')
+def apply_gcj(self):
 	Utils.def_attrs(self, jarname='', jaropts='', classpath='',
 		source_root='.', jar_mf_attributes={}, jar_mf_classpath=[])
+
+	try:
+		self.meths.remove('apply_java')
+	except ValueError:
+		pass
 
 	nodes_lst = []
 
@@ -25,12 +26,14 @@ def apply_java(self):
 	else:
 		self.env['CLASSPATH'] = self.classpath
 
-	re_foo = re.compile(self.source)
+	re_foo = re.compile(self.java_source)
 
 	source_root_node = self.path.find_dir(self.source_root)
 
 	src_nodes = []
 	bld_nodes = []
+
+	all_at_once = getattr(self, 'gcj_once', None)
 
 	prefix_path = source_root_node.abspath()
 	for (root, dirs, filenames) in os.walk(source_root_node.abspath()):
@@ -42,32 +45,35 @@ def apply_java(self):
 
 			if re_foo.search(file) > -1:
 				node = source_root_node.find_resource(file)
-				src_nodes.append(node)
 
-				node2 = node.change_ext(".class")
-				bld_nodes.append(node2)
+				if all_at_once:
+					bld_nodes.append(node)
+				else:
+					node2 = node.change_ext('.o')
 
-	self.env['OUTDIR'] = source_root_node.abspath(self.env)
+					tsk = self.create_task('gcj')
+					tsk.set_inputs(node)
+					tsk.set_outputs(node2)
+					bld_nodes.append(node2)
 
-	tsk = self.create_task('javac')
-	tsk.set_inputs(src_nodes)
-	tsk.set_outputs(bld_nodes)
+	#self.env['OUTDIR'] = source_root_node.abspath(self.env)
 
-	if self.jarname:
-		tsk = self.create_task('jar_create')
-		tsk.set_inputs(bld_nodes)
-		tsk.set_outputs(self.path.find_or_declare(self.jarname))
+	tsk = self.create_task('gcj_link')
+	tsk.set_inputs(bld_nodes)
+	tsk.set_outputs(self.path.find_or_declare(self.target))
+	if getattr(self, 'gcjlinkflags', None):
+		tsk.env.append_unique('GCJLINKFLAGS', self.gcjlinkflags)
 
-		if not self.env['JAROPTS']:
-			if self.jaropts:
-				self.env['JAROPTS'] = self.jaropts
-			else:
-				dirs = '.'
-				self.env['JAROPTS'] = '-C %s %s' % (self.env['OUTDIR'], dirs)
 
-cls = Task.simple_task_type('gcj', '${GCJ} ${GCJFLAGS} -classpath ${CLASSPATH} -d ${OUTDIR} ${SRC}', before='jar_create')
-cls.color = 'BLUE'
+cls = Task.simple_task_type('gcj', '${GCJ} -c -o ${TGT} ${GCJFLAGS} -classpath ${CLASSPATH} ${SRC}')
+cls.before = 'gcj_link jar_create'
+cls.color  = 'BLUE'
+
+cls = Task.simple_task_type('gcj_link', '${GCJLINK} ${GCJLINKFLAGS} ${SRC} -o ${TGT}')
+cls.before = 'jar_create'
+cls.color  = 'GREEN'
 
 def detect(conf):
-	conf.find_program('gcj', var='GCJ', path_list=java_path)
+	conf.find_program('gcj', var='GCJ')
+	conf.env['GCJLINK'] = conf.env['GCJ']
 
