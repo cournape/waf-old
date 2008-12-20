950ac18b7357779b2fb7834e470514719eb18741
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2008-12-20 23:58:29 +0000

    Show how to extract dependencies using gcc.
    git-svn-id: http://waf.googlecode.com/svn/trunk@5166 f0382ac9-c320-0410-b3f0-b508d59f5a85

---
+++ playground/gccdeps/gccdeps.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Thomas Nagy, 2008 (ita)
+
+import Task, Utils
+import preproc
+
+ccvars = "CC CCFLAGS CPPFLAGS _CCINCFLAGS _CCDEFFLAGS".split()
+cxxvars = "CXX CXXFLAGS CPPFLAGS _CXXINCFLAGS _CXXDEFFLAGS".split()
+
+def c_fun(tsk, vars):
+	vars = [tsk.env.get_flat(k) for k in vars]
+	vars.append('-M')
+	vars.append(tsk.inputs[0].abspath(tsk.env))
+	return " ".join(vars)
+
+def scan(self):
+	if self.__class__.__name__ == 'cxx':
+		vars = cxxvars
+	else:
+		vars = ccvars
+
+	deps = Utils.cmd_output(c_fun(self, vars))
+
+	deps = deps.replace('\\\n', '')
+	deps = ":".join(deps.split(':')[1:])
+	deps = deps.split()
+
+	nodes = [self.generator.bld.root.find_resource(x) for x in deps]
+
+	return (nodes, [])
+
+t = Task.TaskBase.classes
+if 'cc' in t:
+	t['cc'].scan = scan
+
+if 'cxx' in t:
+	t['cxx'].scan = scan
+
---
+++ playground/gccdeps/main.c
@@ -0,0 +1,17 @@
+#include "a/a.h"
+#include "b/b.h"
+
+int main()
+{
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
