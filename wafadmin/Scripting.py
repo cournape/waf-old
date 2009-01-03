935a2cbcb89733094322ec4d2f72518c02924dd7
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-01-03 03:34:47 +0000

    Change 'compilation' into 'build'
    git-svn-id: http://waf.googlecode.com/svn/trunk@5333 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Scripting.py
+++ wafadmin/Scripting.py
@@ -282,7 +282,7 @@ def main():
 			ela = time.strftime(' (%H:%M:%S)', time.gmtime(time.time() - ini))
 		if Options.commands['install']: msg = 'Compilation and installation finished successfully%s' % ela
 		elif Options.commands['uninstall']: msg = 'Uninstallation finished successfully%s' % ela
-		else: msg = 'Compilation finished successfully%s' % ela
+		else: msg = 'Build finished successfully%s' % ela
 		info(msg)
 
 	# clean
