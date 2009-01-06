84b38c1eacf09370faa9a672547b09c0bfbad2f8
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-01-06 19:01:50 +0000

    When python is >= 2.6, use the system subprocess module
    git-svn-id: http://waf.googlecode.com/svn/trunk@5361 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Utils.py
+++ wafadmin/Utils.py
@@ -36,8 +36,11 @@ Utilities, the stable ones are the following:
 import os, sys, imp, string, errno, traceback, inspect, re
 try: from UserDict import UserDict
 except ImportError: from collections import UserDict
-try: import pproc
-except: import subprocess as pproc
+if sys.hexversion > 0x2060000:
+	import subprocess as pproc
+else:
+	try: import pproc
+	except: import subprocess as pproc
 import Logs
 from Constants import *
 
