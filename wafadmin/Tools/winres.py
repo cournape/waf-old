1df5901bd781f556e0369574c3a2fea970a76288
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-01-13 18:53:13 +0000

    Update.
    git-svn-id: http://waf.googlecode.com/svn/trunk@5441 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/winres.py
+++ wafadmin/Tools/winres.py
@@ -16,7 +16,7 @@ winrc_str = '${WINRC} ${_CPPDEFFLAGS} ${_CXXDEFFLAGS} ${_CCDEFFLAGS} ${WINRCFLAG
 @extension(EXT_WINRC)
 def rc_file(self, node):
 	obj_ext = '.rc.o'
-	if self.env['WINRC_TGT_F'] == '/fo ': obj_ext = '.res'
+	if self.env['WINRC_TGT_F'] == '/fo': obj_ext = '.res'
 
 	rctask = self.create_task('winrc')
 	rctask.set_inputs(node)
@@ -34,26 +34,20 @@ def detect(conf):
 	cc = os.path.basename(''.join(v['CC']).lower())
 	cxx = os.path.basename(''.join(v['CXX']).lower())
 
-	# remove trailing " from windows paths
-	cc=re.sub('"', '',cc)
-	cxx=re.sub('"', '',cxx)
-
 	# find rc.exe
 	if cc in ['gcc', 'cc', 'g++', 'c++']:
-		winrc = conf.find_program('windres', var='WINRC')
-		v['WINRC_TGT_F'] = '-o '
-		v['WINRC_SRC_F'] = '-i '
+		winrc = conf.find_program('windres', var='WINRC', path_list = v['PATH'])
+		v['WINRC_TGT_F'] = '-o'
+		v['WINRC_SRC_F'] = '-i'
 	elif cc.startswith('cl.exe') or cxx.startswith('cl.exe'):
-		winrc = conf.find_program('RC', var='WINRC')
-		v['WINRC_TGT_F'] = '/fo '
-		v['WINRC_SRC_F'] = ' '
+		winrc = conf.find_program('RC', var='WINRC', path_list = v['PATH'])
+		v['WINRC_TGT_F'] = '/fo'
+		v['WINRC_SRC_F'] = ''
 	else:
 		winrc = None
 
 	if not winrc:
-		conf.fatal('winrc was not found!!')
-	else:
-		v['WINRC'] = quote_whitespace(winrc)
+		conf.fatal('winrc was not found!')
 
 	v['WINRCFLAGS'] = ''
 
