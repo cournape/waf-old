41de38ab7c58639cee0c5c2d2249d3e2f9f025a7
cournape <cournape@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-02-01 07:57:56 +0000

    Put fortran configuration stuff in separate file.
    git-svn-id: http://waf.googlecode.com/svn/trunk@5658 f0382ac9-c320-0410-b3f0-b508d59f5a85

---
+++ playground/fortran/config_fortran.py
@@ -0,0 +1,348 @@
+import re
+import shutil
+import os
+import sys
+
+from Configure import conftest, conf
+
+from myconfig import MyBuildContext
+
+GCC_DRIVER_LINE = re.compile('^Driving:')
+POSIX_STATIC_EXT = re.compile('\S+\.a')
+POSIX_LIB_FLAGS = re.compile('-l\S+')
+
+def _got_link_verbose_posix(lines):
+	"""Returns true if useful link options can be found in output.
+
+	POSIX implementation.
+
+	Expect lines to be a list of lines."""
+	for line in lines:
+		if not GCC_DRIVER_LINE.search(line):
+			if POSIX_STATIC_EXT.search(line) or POSIX_LIB_FLAGS.search(line):
+				return True
+	return False
+
+def _got_link_verbose(lines):
+	"""Return true if useful link option can be found in output."""
+	if sys.platform == 'win32':
+		raise NotImplementedError("FIXME: not implemented on win32")
+	else:
+		return _got_link_verbose_posix(lines)
+
+@conf
+def check_fortran_verbose(self, autoadd=True, *k, **kw):
+	kw["compile_filename"] = "test.f"
+	kw["code"] = """\
+       PROGRAM MAIN
+       END
+	"""
+
+	kw['compile_mode'] = 'fortran'
+	kw['type'] = 'fprogram'
+	kw['env'] = self.env.copy()
+	kw['execute'] = 0
+
+	kw['msg'] = kw.get('msg', 'Getting fortran link verbose flag')
+	kw['errmsg'] = kw.get('errmsg', 'bad luck')
+
+	self.check_message_1(kw['msg'])
+	flags = ['-v', '--verbose', '-verbose', '-V']
+	gotflag = False
+	for flag in flags:
+		kw['env']['LINKFLAGS'] = flag
+		try:
+			ret, out = self.mycompile_code(*k, **kw)
+		except:
+			ret = 1
+			out = ""
+			
+		if ret == 0 and _got_link_verbose(out.splitlines()):
+			gotflag = True
+			break
+
+	if gotflag:
+		self.check_message_2('ok (%s)' % flag, 'GREEN')
+		if autoadd:
+			self.env["FC_VERBOSE_FLAG"] = flag	
+	else:
+		self.check_message_2(kw['errmsg'], 'YELLOW')
+
+	return ret
+
+# Get Fortran runtime libraries
+import shlex
+
+# linkflags which match those are ignored
+LINKFLAGS_IGNORED = [r'-lang*', r'-lcrt[a-zA-Z0-9]*\.o', r'-lc$', r'-lSystem',
+                     r'-libmil', r'-LIST:*', r'-LNO:*']
+if os.name == 'nt':
+	LINKFLAGS_IGNORED.extend([r'-lfrt*', r'-luser32',
+			r'-lkernel32', r'-ladvapi32', r'-lmsvcrt',
+			r'-lshell32', r'-lmingw', r'-lmoldname'])
+else:
+	LINKFLAGS_IGNORED.append(r'-lgcc*')
+
+RLINKFLAGS_IGNORED = [re.compile(f) for f in LINKFLAGS_IGNORED]
+
+def _match_ignore(line):
+	"""True if the line should be ignored."""
+	if [i for i in RLINKFLAGS_IGNORED if i.match(line)]:
+		return True
+	else:
+		return False
+
+def parse_fortran_link(lines):
+	"""Given the output of verbose link of Fortran compiler, this returns a
+	list of flags necessary for linking using the standard linker."""
+	# TODO: On windows ?
+	final_flags = []
+	for line in lines:
+		if not GCC_DRIVER_LINE.match(line):
+			_parse_flink_line(line, final_flags)
+	return final_flags
+
+SPACE_OPTS = re.compile('^-[LRuYz]$')
+NOSPACE_OPTS = re.compile('^-[RL]')
+
+def _parse_flink_line(line, final_flags):
+	lexer = shlex.shlex(line, posix = True)
+	lexer.whitespace_split = True
+
+	t = lexer.get_token()
+	tmp_flags = []
+	while t:
+		def parse(token):
+			# Here we go (convention for wildcard is shell, not regex !)
+			#   1 TODO: we first get some root .a libraries
+			#   2 TODO: take everything starting by -bI:*
+			#   3 Ignore the following flags: -lang* | -lcrt*.o | -lc |
+			#   -lgcc* | -lSystem | -libmil | -LANG:=* | -LIST:* | -LNO:*)
+			#   4 take into account -lkernel32
+			#   5 For options of the kind -[[LRuYz]], as they take one argument
+			#   after, the actual option is the next token 
+			#   6 For -YP,*: take and replace by -Larg where arg is the old
+			#   argument
+			#   7 For -[lLR]*: take
+
+			# step 3
+			if _match_ignore(token):
+				pass
+			# step 4
+			elif token.startswith('-lkernel32') and sys.platform == 'cygwin':
+				tmp_flags.append(token)
+			# step 5
+			elif SPACE_OPTS.match(token):
+				t = lexer.get_token()
+				if t.startswith('P,'):
+					t = t[2:]
+				for opt in t.split(os.pathsep):
+					tmp_flags.append('-L%s' % opt)
+			# step 6
+			elif NOSPACE_OPTS.match(token):
+				tmp_flags.append(token)
+			# step 7
+			elif POSIX_LIB_FLAGS.match(token):
+				tmp_flags.append(token)
+			else:
+				# ignore anything not explicitely taken into account
+				pass
+
+			t = lexer.get_token()
+			return t
+		t = parse(t)
+
+	final_flags.extend(tmp_flags)
+	return final_flags
+
+@conf
+def check_fortran_clib(self, autoadd=True, *k, **kw):
+	# Get verbose flag
+	try:
+		flag = self.env["FC_VERBOSE_FLAG"]
+		if len(flag) < 1:
+			raise KeyError
+	except KeyError:
+		if self.check_fortran_verbose():
+			return 1
+
+	flag = self.env["FC_VERBOSE_FLAG"]
+
+	kw["compile_filename"] = "test.f"
+	kw["code"] = """\
+       PROGRAM MAIN
+       END
+	"""
+
+	kw['compile_mode'] = 'fortran'
+	kw['type'] = 'fprogram'
+	kw['env'] = self.env.copy()
+	kw['execute'] = 0
+
+	kw['msg'] = kw.get('msg', 'Getting fortran runtime link flags')
+	kw['errmsg'] = kw.get('errmsg', 'bad luck')
+
+	self.check_message_1(kw['msg'])
+	kw['env']['LINKFLAGS'] = flag
+	try:
+		ret, out = self.mycompile_code(*k, **kw)
+	except:
+		ret = 1
+		out = ""
+
+	if ret == 0:
+		flags = parse_fortran_link(out.splitlines())
+
+	if ret == 0:
+		self.check_message_2('ok (%s)' % " ".join(flags), 'GREEN')
+		if autoadd:
+			self.env["FC_CLIB_LDFLAGS"] = flag	
+	else:
+		self.check_message_2(kw['errmsg'], 'YELLOW')
+
+	return ret
+
+def compile_subroutines(self, *k, **kw):
+	kw["compile_filename"] = "subroutines.f"
+	kw["code"] = """\
+      subroutine foobar()
+      return
+      end
+      subroutine foo_bar()
+      return
+      end
+"""
+
+	kw['compile_mode'] = 'fortran'
+	kw['type'] = 'fstaticlib'
+	kw['env'] = self.env.copy()
+	kw['execute'] = 0
+
+	#kw['msg'] = kw.get('msg', 'Getting fortran runtime link flags')
+	kw['errmsg'] = kw.get('errmsg', 'bad luck')
+
+	try:
+		ret, out = self.mycompile_code(*k, **kw)
+	except:
+		ret = 1
+
+	return ret
+
+@conf
+def check_fortran_mangling(self, *k, **kw):
+	compile_subroutines(self, *k, **kw)
+#    env = self.env
+#    main_tmpl = """
+#      int %s() { return 1; }
+#"""
+#    prog_tmpl = """
+#      void %s(void);
+#      void %s(void);
+#      int my_main() {
+#      %s();
+#      %s();
+#      return 0;
+#      }
+#"""
+    # variants:
+    #   lower-case, no underscore, no double underscore: foobar, foo_bar
+    #   ...
+    #   upper-case, underscore, double underscore: FOOBAR_, FOO_BAR__
+    #context.TryCompile(subr, ext)
+    #obj = context.lastTarget
+    #env.Append(LIBS = env.StaticLibrary(obj))
+    #under = ['', '_']
+    #doubleunder = ['', '_']
+    #casefcn = ["lower", "upper"]
+    #gen = _RecursiveGenerator(under, doubleunder, casefcn)
+    #while True:
+    #    try:
+    #        u, du, c = gen.next()
+    #        def make_mangler(u, du, c):
+    #            return lambda n: getattr(string, c)(n) +\
+    #                             u + (n.find('_') != -1 and du or '')
+    #        mangler = make_mangler(u, du, c)
+    #        foobar = mangler("foobar")
+    #        foo_bar = mangler("foo_bar")
+    #        prog = prog_tmpl % (foobar, foo_bar, foobar, foo_bar)
+    #        if m:
+    #            prog = main_tmpl % m + prog
+    #        result = context.TryLink(prog, '.c')
+    #        if result:
+    #            break
+    #    except StopIteration:
+    #        result = mangler = u = du = c = None
+    #        break
+
+    #return result, mangler, u, du, c
+
+# XXX: things which have nothing to do here...
+@conftest
+def mycompile_code(self, *k, **kw):
+	"""Like compile_code, but return output of the compiler command as well as
+	the return code."""
+	test_f_name = kw['compile_filename']
+
+	# create a small folder for testing
+	dir = os.path.join(self.blddir, '.wscript-trybuild')
+
+	# if the folder already exists, remove it
+	try:
+		shutil.rmtree(dir)
+	except OSError:
+		pass
+	os.makedirs(dir)
+
+	bdir = os.path.join(dir, 'testbuild')
+
+	if not os.path.exists(bdir):
+		os.makedirs(bdir)
+
+	env = kw['env']
+
+	dest = open(os.path.join(dir, test_f_name), 'w')
+	dest.write(kw['code'])
+	dest.close()
+
+	back = os.path.abspath('.')
+
+	bld = MyBuildContext()
+	bld.log = self.log
+	bld.all_envs.update(self.all_envs)
+	bld.all_envs['default'] = env
+	bld.lst_variants = bld.all_envs.keys()
+	bld.load_dirs(dir, bdir)
+
+	os.chdir(dir)
+
+	bld.rescan(bld.srcnode)
+
+	o = bld.new_task_gen(features=[kw['compile_mode'], kw['type']],
+			source=test_f_name, target='testprog')
+
+	for k, v in kw.iteritems():
+		setattr(o, k, v)
+
+	self.log.write("==>\n%s\n<==\n" % kw['code'])
+
+	# compile the program
+	try:
+		bld.compile()
+	except:
+		ret = Utils.ex_stack()
+	else:
+		ret = 0
+
+	# chdir before returning
+	os.chdir(back)
+
+	if ret:
+		self.log.write('command returned %r' % ret)
+		self.fatal(str(ret))
+
+	# keep the name of the program to execute
+	if kw['execute']:
+		lastprog = o.link_task.outputs[0].abspath(env)
+
+	return ret, bld.out
+
