e3ccf269d6517c8bd85ce5d16cfe1c0b75aad64e
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-02-22 08:54:53 +0000

    Better example
    git-svn-id: http://waf.googlecode.com/svn/trunk@5758 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- demos/d/example.d
+++ demos/d/example.d
@@ -6,24 +6,30 @@ import std .  // system2;
 system  ;
 static import std.date, std.thread /+ /+ +/ , std.io +/ ;
 
+import testlib.code;
+
 int main(string[] args)   // string is a type alias for const(char)[]
 {
-    // Declare an associative array with string keys and
-    // arrays of strings as data
-    char[][] [char[]] container;
+	// Declare an associative array with string keys and
+	// arrays of strings as data
+	char[][] [char[]] container;
 
-    // Add some people to the container and let them carry some items
-    container["Anya"] ~= "scarf";
-    container["Dimitri"] ~= "tickets";
-    container["Anya"] ~= "puppy";
+	int result = test_lib();
 
-    // Iterate over all the persons in the container
-    foreach (char[] person, char[][] items; container)
-        display_item_count(person, items);
-    return 0;
+
+	// Add some people to the container and let them carry some items
+	container["Anya"] ~= "scarf";
+	container["Dimitri"] ~= "tickets";
+	container["Anya"] ~= "puppy";
+
+	// Iterate over all the persons in the container
+	foreach (char[] person, char[][] items; container)
+		display_item_count(person, items);
+	return 0;
 }
 
 void display_item_count(char[] person, char[][] items)
 {
-    writefln(person, " is carrying ", items.length, " items.");
+	writefln(person, " is carrying ", items.length, " items");
 }
+
