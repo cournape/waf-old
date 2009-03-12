66bb3e39cc37b05d728c87f47848b52cd5b5076c
yinon.me <yinon.me@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-03-12 15:25:47 +0000

    close #382
    git-svn-id: http://waf.googlecode.com/svn/trunk@5849 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/ar.py
+++ wafadmin/Tools/ar.py
@@ -7,11 +7,8 @@
 
 import os, sys
 import Task
+import Utils
 from Configure import conftest
-
-ar_str = '${AR} ${ARFLAGS} ${TGT} ${SRC}'
-cls = Task.simple_task_type('ar_link_static', ar_str, color='YELLOW', ext_in='.o')
-cls.maxjobs = 1
 
 def detect(conf):
 	comp = conf.find_program('ar', var='AR')
@@ -32,4 +29,31 @@ def find_ar(conf):
 	conf.check_tool('ar')
 	if not v['AR']: conf.fatal('ar is required for static libraries - not found')
 
+def ar_link_static(task):
+	env = task.env
+
+	# always remove the archive - prevents remnants of old object files
+	# (renamed, deleted, etc.)
+	tgt = task.outputs[0].bldpath(env)
+	try: os.remove(tgt)
+	except: pass
+
+#	Windows:
+#		ar_str = '${AR} s${ARFLAGS} ${TGT} ${SRC}'
+#	Others:
+#		ar_str = '${AR} ${ARFLAGS} ${TGT} ${SRC} && ${RANLIB} ${RANLIBFLAGS} ${TGT}'
+
+	ar_flags = env['ARFLAGS']
+	ranlib = ''
+	if Utils.is_win32:
+		ar_flags[0] = 's' + ar_flags[0]
+	else:
+		ranlib = ' && %s %s %s' % (env['RANLIB'], env['RANLIBFLAGS'], tgt)
+
+	srcs = [a.srcpath(env) for a in task.inputs]
+	command = '%s %s %s %s%s' % (env['AR'], ar_flags, tgt, ' '.join(srcs), ranlib)
+	return task.exec_command(command)
+
+cls = Task.task_type_from_func('ar_link_static', color='YELLOW', func=ar_link_static, ext_in='.o')
+cls.maxjobs = 1
 
