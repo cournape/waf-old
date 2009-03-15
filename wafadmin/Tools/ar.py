9d80823ddb93a36fcc5b33941ba04117cefdd5da
yinon.me <yinon.me@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-03-15 09:12:36 +0000

    close #382
    git-svn-id: http://waf.googlecode.com/svn/trunk@5871 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/ar.py
+++ wafadmin/Tools/ar.py
@@ -14,15 +14,12 @@ cls = Task.simple_task_type('ar_link_static', ar_str, color='YELLOW', ext_in='.o
 cls.maxjobs = 1
 
 # remove the output in case it already exists
-# yinon: you may uncomment this once you have a proof it is necessary
-"""
 old = cls.run
 def wrap(self):
 	try: os.remove(self.outputs[0].abspath(self.env))
 	except OSError: pass
 	return old(self)
 setattr(cls, 'run', wrap)
-"""
 
 def detect(conf):
 	comp = conf.find_program('ar', var='AR')
