ef9dad66e3ea77d7e35108def206f287dc8f458e
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-04-03 14:04:43 +0000

    Indentation, cleanup
    git-svn-id: http://waf.googlecode.com/svn/trunk@5964 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- playground/cross/cross_linux_win32.py
+++ playground/cross/cross_linux_win32.py
@@ -12,8 +12,8 @@ def find_mingw_cc(conf):
 	v['CC'] = None
 	cc = conf.find_program('mingw32-gcc', var='CC')
 	if not cc: cc = conf.find_program('mingw32-cc', var='CC')
-        if not cc: cc = conf.find_program('i586-mingw32msvc-gcc', var='CC')
-        if not cc: cc = conf.find_program('i586-mingw32msvc-cc', var='CC')
+	if not cc: cc = conf.find_program('i586-mingw32msvc-gcc', var='CC')
+	if not cc: cc = conf.find_program('i586-mingw32msvc-cc', var='CC')
 	if not cc: conf.fatal('mingw32-gcc was not found')
 	try:
 		if Utils.cmd_output('%s --version' % cc).find('mingw') < 0:
@@ -29,7 +29,7 @@ def find_mingw_ar(conf):
 	v = conf.env
 	v['AR'] = None
 	ar = conf.find_program('mingw32-ar', var='AR')
-        if not ar: ar = conf.find_program('i586-mingw32msvc-ar', var='AR')
+	if not ar: ar = conf.find_program('i586-mingw32msvc-ar', var='AR')
 	if not ar: conf.fatal('mingw32-ar was not found')
 	try:
 		if Utils.cmd_output('%s --version' % ar).find('ar') < 0:
@@ -43,7 +43,7 @@ def find_mingw_ranlib(conf):
 	v = conf.env
 	v['RANLIB'] = None
 	ranlib = conf.find_program('mingw32-ranlib', var='RANLIB')
-        if not ranlib: ranlib = conf.find_program('i586-mingw32msvc-ranlib', var='RANLIB')
+	if not ranlib: ranlib = conf.find_program('i586-mingw32msvc-ranlib', var='RANLIB')
 	if not ranlib: conf.fatal('mingw32-ranlib was not found')
 	try:
 		if Utils.cmd_output('%s --version' % ranlib).find('ranlib') < 0:
@@ -58,9 +58,9 @@ def find_mingw_cxx(conf):
 	v = conf.env
 	v['CXX'] = None
 	cxx = conf.find_program('mingw32-g++', var='CXX')
-        if not cxx: cxx = conf.find_program('mingw32-c++', var='CXX')
-        if not cxx: cxx = conf.find_program('i586-mingw32msvc-g++', var='CXX')
-        if not cxx: cxx = conf.find_program('i586-mingw32msvc-c++', var='CXX')
+	if not cxx: cxx = conf.find_program('mingw32-c++', var='CXX')
+	if not cxx: cxx = conf.find_program('i586-mingw32msvc-g++', var='CXX')
+	if not cxx: cxx = conf.find_program('i586-mingw32msvc-c++', var='CXX')
 	if not cxx: conf.fatal('mingw32-g++ was not found')
 	try:
 		if Utils.cmd_output('%s --version' % cxx).find('mingw') < 0:
@@ -76,9 +76,9 @@ def find_mingw_cpp(conf):
 	v = conf.env
 	v['CPP'] = None
 	cpp = conf.find_program('mingw32-g++', var='CPP')
-        if not cpp: cpp = conf.find_program('mingw32-c++', var='CPP')
-        if not cpp: cpp = conf.find_program('i586-mingw32msvc-g++', var='CPP')
-        if not cpp: cpp = conf.find_program('i586-mingw32msvc-c++', var='CPP')
+	if not cpp: cpp = conf.find_program('mingw32-c++', var='CPP')
+	if not cpp: cpp = conf.find_program('i586-mingw32msvc-g++', var='CPP')
+	if not cpp: cpp = conf.find_program('i586-mingw32msvc-c++', var='CPP')
 	if not cpp: conf.fatal('mingw32-g++ was not found')
 	try:
 		if Utils.cmd_output('%s --version' % cpp).find('mingw') < 0:
@@ -89,5 +89,18 @@ def find_mingw_cpp(conf):
 	v['CPP_NAME'] = 'gcc'
 	ccroot.get_cc_version(conf, [cpp], 'CPP_VERSION')
 
-detect = 'find_mingw_ar find_mingw_ranlib find_mingw_cc find_mingw_cpp find_mingw_cxx'
+@conftest
+def mingw_flags(conf):
+	# As we have to change target platform after the tools
+	# have been loaded there are a few variables that needs
+	# to be initiated if building for win32.
+	# Make sure we don't have -fPIC and/or -DPIC in our CCFLAGS
+	conf.env['shlib_CXXFLAGS'] = []
+
+	# Setup various target file patterns
+	conf.env['staticlib_PATTERN'] = '%s.lib'
+	conf.env['shlib_PATTERN'] = '%s.dll'
+	conf.env['program_PATTERN'] = '%s.exe'
+
+detect = 'find_mingw_ar find_mingw_ranlib find_mingw_cc find_mingw_cpp find_mingw_cxx mingw_flags'
 
