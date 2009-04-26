e590bd099a3615863d46d9a214c583e18939ba74
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-04-26 11:45:48 +0000

    dumb dependency scanner
    git-svn-id: http://waf.googlecode.com/svn/trunk@6153 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/preproc.py
+++ wafadmin/Tools/preproc.py
@@ -748,7 +748,63 @@ class c_parser(object):
 				self.ban_includes.append(self.curfile)
 
 def get_deps(node, env, nodepaths=[]):
+	"""
+	Get the dependencies using a c/c++ preprocessor, this is required for finding dependencies of the kind
+	#include some_macro()
+	"""
+
 	gruik = c_parser(nodepaths)
 	gruik.start(node, env)
 	return (gruik.nodes, gruik.names)
+
+#################### dumb dependency scanner
+
+re_inc = re.compile(\
+	'^[ \t]*(#|%:)[ \t]*(include)[ \t]*(.*)\r*$',
+	re.IGNORECASE | re.MULTILINE)
+
+def lines_includes(filename):
+	code = Utils.readf(filename)
+	if use_trigraphs:
+		for (a, b) in trig_def: code = code.split(a).join(b)
+	code = re_nl.sub('', code)
+	code = re_cpp.sub(repl, code)
+	return [(m.group(2), m.group(3)) for m in re.finditer(re_inc, code)]
+
+def get_deps(node, env, nodepaths=[], defines={}):
+	"""
+	Get the dependencies by just looking recursively at the #include statements
+	"""
+
+	nodes = []
+	names = []
+
+	def find_deps(node):
+		lst = lines_includes(node.abspath(env))
+
+		for (_, line) in lst:
+			(t, filename) = extract_include(line, defines)
+			if filename in names:
+				continue
+
+			if filename.endswith('.moc'):
+				names.append(filename)
+
+			found = None
+			for n in nodepaths:
+				if found:
+					break
+				found = n.find_resource(filename)
+
+			if not found:
+				if not filename in names:
+					names.append(filename)
+			elif not found in nodes:
+				nodes.append(found)
+				find_deps(node)
+
+	find_deps(node)
+	return (nodes, names)
+
+
 
