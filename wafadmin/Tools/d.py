e4669f8e8d6423789c5ac5c76314eb6ec7caca8f
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-05-26 18:32:18 +0000

    for #458
    git-svn-id: http://waf.googlecode.com/svn/trunk@6282 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/d.py
+++ wafadmin/Tools/d.py
@@ -439,7 +439,7 @@ link_str = '${D_LINKER} ${DLNK_SRC_F}${SRC} ${DLNK_TGT_F}${TGT} ${DLINKFLAGS}'
 cls = Task.simple_task_type('d', d_str, 'GREEN', before='ar_link_static d_link', shell=False)
 cls.scan = scan
 Task.simple_task_type('d_with_header', d_with_header_str, 'GREEN', before='ar_link_static d_link', shell=False)
-Task.simple_task_type('d_link', link_str, color='YELLOW', shell=False)
+Task.simple_task_type('d_link', link_str, color='YELLOW', shell=True) # TODO stupid dmd wants -offoo without spaces
 
 # for feature request #104
 @taskgen
