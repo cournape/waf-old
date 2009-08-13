8294adec3e573f3b268b512b75e04c00fc221601
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-08-13 16:33:53 +0000

    provide a symbol for the link
    git-svn-id: http://waf.googlecode.com/svn/trunk@6529 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/cxx.py
+++ wafadmin/Tools/cxx.py
@@ -100,12 +100,12 @@ cls.scan = ccroot.scan
 cls.vars.append('CXXDEPS')
 
 link_str = '${LINK_CXX} ${CXXLNK_SRC_F}${SRC} ${CXXLNK_TGT_F}${TGT} ${LINKFLAGS}'
-cls = Task.simple_task_type('cxx_link', link_str, color='YELLOW', ext_in='.o', shell=False)
+cls = Task.simple_task_type('cxx_link', link_str, color='YELLOW', ext_in='.o', ext_out='.bin', shell=False)
 cls.maxjobs = 1
-cls2 = Task.task_type_from_func('vnum_cxx_link', ccroot.link_vnum, cls.vars, color='CYAN', ext_in='.o')
+cls2 = Task.task_type_from_func('vnum_cxx_link', ccroot.link_vnum, cls.vars, color='CYAN', ext_in='.o', ext_out='.bin')
 cls2.maxjobs = 1
 
 link_str = '${LINK_CXX} ${CXXLNK_SRC_F}${SRC} ${CXXLNK_TGT_F}${TGT[0].abspath(env)} ${LINKFLAGS}'
-cls = Task.simple_task_type('dll_cxx_link', link_str, color='YELLOW', ext_in='.o', shell=False)
+cls = Task.simple_task_type('dll_cxx_link', link_str, color='YELLOW', ext_in='.o', ext_out='.bin', shell=False)
 cls.maxjobs = 1
 
