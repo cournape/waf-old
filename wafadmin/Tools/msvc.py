34cb879e275d7c0045e6de7056169971d2780eb9
mercier.nicolas@gmail.com <mercier.nicolas@gmail.com@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-08-17 09:45:08 +0000

    removed useless buggy line for PDB install
    git-svn-id: http://waf.googlecode.com/svn/trunk@6598 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/msvc.py
+++ wafadmin/Tools/msvc.py
@@ -642,7 +642,6 @@ def apply_flags_msvc(self):
 				pdbnode = self.link_task.outputs[0].change_ext('.pdb')
 				pdbfile = pdbnode.bldpath(self.env)
 				self.link_task.outputs.append(pdbnode)
-				bld.install_files('${BINDIR}', pdbnode, env=self.env)
 				break
 
 @feature('cprogram', 'cshlib', 'cstaticlib')
