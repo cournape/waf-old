bf39d0cf59e9a206b9e36db67259dc9619fb979f
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-08-29 20:28:17 +0000

    for #243
    git-svn-id: http://waf.googlecode.com/svn/trunk@6648 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Logs.py
+++ wafadmin/Logs.py
@@ -31,7 +31,7 @@ if got_tty:
 
 import Utils
 
-if not got_tty or sys.platform == 'win32' or 'NOCOLOR' in os.environ:
+if not got_tty or 'NOCOLOR' in os.environ:
 	colors_lst['USE'] = False
 
 def get_color(cl):
@@ -125,4 +125,59 @@ def init_log():
 
 # may be initialized more than once
 init_log()
+
+
+# win32 console, grrrr
+try:
+	from ctypes import *
+except ImportError:
+	pass
+else:
+	import re
+
+	STD_OUTPUT_HANDLE = -11
+	STD_ERROR_HANDLE = -12
+
+	escape_to_color = { (0, 30): 0x0,			 #black
+						(0, 31): 0x4,			 #red
+						(0, 32): 0x2,			 #green
+						(0, 33): 0x4+0x2,		 #dark yellow
+						(0, 34): 0x1,			 #blue
+						(0, 35): 0x1+0x4,		 #purple
+						(0, 36): 0x2+0x4,		 #cyan
+						(0, 37): 0x1+0x2+0x4,	 #grey
+						(1, 30): 0x1+0x2+0x4,	 #dark gray
+						(1, 31): 0x4+0x8,		 #red
+						(1, 32): 0x2+0x8,		 #light green
+						(1, 33): 0x4+0x2+0x8,	 #yellow
+						(1, 34): 0x1+0x8,		 #light blue
+						(1, 35): 0x1+0x4+0x8,	 #light purple
+						(1, 36): 0x1+0x2+0x8,	 #light cyan
+						(1, 37): 0x1+0x2+0x4+0x8, #white
+		}
+
+	class AnsiTerm(object):
+		def __init__(self):
+			self.hconsole = windll.kernel32.GetStdHandle(type)
+
+		ansi_tokens = re.compile('(?:\x1b\[([0-9;]*)([a-zA-Z])|([^\x1b]+))')
+		def write(self, text):
+			for param, cmd, txt in self.ansi_tokens.findall(text):
+				if cmd:
+					intensity, sep, color = param.partition(';')
+					intensity = int(intensity)
+					color = int(color)
+					attrib = self.escape_to_color.get((intensity, color), 0x7)
+					windll.kernel32.SetConsoleTextAttribute(self.hconsole, attrib)
+				else:
+					chars_written = c_int()
+					windll.kernel32.WriteConsoleA(self.hconsole, txt, len(txt), byref(chars_written), None)
+
+		def flush(self):
+			pass
+
+	sys.stdout = AnsiTerm(STD_OUTPUT_HANDLE)
+	sys.stderr = AnsiTerm(STD_ERROR_HANDLE)
+	os.environ['TERM'] = 'vt100'
+
 
