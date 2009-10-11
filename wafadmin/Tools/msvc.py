aa7ffca7da9136c97d91918b0337106b83482424
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-10-11 09:29:15 +0000

    #547
    git-svn-id: http://waf.googlecode.com/svn/trunk@6749 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/msvc.py
+++ wafadmin/Tools/msvc.py
@@ -604,7 +604,9 @@ def msvc_common_flags(conf):
 	v['STATICLIB_ST']     = 'lib%s.lib' # Note: to be able to distinguish between a static lib and a dll import lib, it's a good pratice to name the static lib 'lib%s.lib' and the dll import lib '%s.lib'
 	v['STATICLIBPATH_ST'] = '/LIBPATH:%s'
 
-	v['LINKFLAGS']        = ['/NOLOGO', '/MANIFEST']
+	v['LINKFLAGS']            = ['/NOLOGO', '/MANIFEST']
+	v['LINKFLAGS_DEBUG']      = ['/DEBUG']
+	v['LINKFLAGS_ULTRADEBUG'] = ['/DEBUG']
 
 	# shared library
 	v['shlib_CCFLAGS']  = ['']
