4ebfed7b51826dc11b502b3a9d8c22ea7dcdc3b2
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2009-10-25 22:09:29 +0000

    i should never let this in waf
    git-svn-id: http://waf.googlecode.com/svn/trunk@6777 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/ccroot.py
+++ wafadmin/Tools/ccroot.py
@@ -613,6 +613,7 @@ def exec_vnum_link(self):
 cls = Task.task_type_from_func('vnum', func=exec_vnum_link, ext_in='.bin', color='CYAN')
 cls.quiet = 1
 
+"""
 # ============ workaround for spurious library dependencies added by apply_lib_vars (elf/gcc-specific) =========
 
 @feature('cshlib', 'cprogram')
@@ -623,3 +624,6 @@ def add_as_needed(self):
 		# All ELF platforms are impacted but only the gcc compiler has a flag to fix it.
 		if 'gcc' in (env.CXX_NAME, env.CC_NAME):
 			env.prepend_value('LINKFLAGS', '-Wl,--as-needed')
+
+"""
+
