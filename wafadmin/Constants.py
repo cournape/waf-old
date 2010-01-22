99f5b39723f49dcdede29988e05f33cd9f8386fe
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-01-22 23:53:30 +0000

    Version increment.
    git-svn-id: http://waf.googlecode.com/svn/trunk@6998 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Constants.py
+++ wafadmin/Constants.py
@@ -9,9 +9,9 @@ maintainer: the version number is updated from the top-level wscript file
 """
 
 # do not touch these three lines, they are updated automatically
-HEXVERSION = 0x105011
-WAFVERSION="1.5.11"
-WAFREVISION = "6830M"
+HEXVERSION = 0x105012
+WAFVERSION="1.5.12"
+WAFREVISION = "6995:6997M"
 ABI = 7
 
 # permissions
