c32d288caea0a69313dbd61bc8ce410a78fbd0e5
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-03-14 15:22:15 +0000

    this scheme should be faster, but it does not protect against creating the same objects several times
    git-svn-id: http://waf.googlecode.com/svn/trunk@7400 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- playground/batched/batched_cc.py
+++ playground/batched/batched_cc.py
@@ -20,8 +20,6 @@ it is only necessary to import this module in the configuration (no other change
 """
 
 MAX_BATCH = 50
-USE_SHELL = False
-GCCDEPS = True
 MAXPARALLEL = False
 
 EXT_C = ['.c', '.cc', '.cpp', '.cxx']
@@ -31,48 +29,24 @@ import TaskGen, Task, ccroot, Build, Logs
 from TaskGen import extension, feature, before
 from Constants import *
 
-if GCCDEPS:
-	lock = threading.Lock()
+cc_str = '${CC} ${CCFLAGS} ${CPPFLAGS} ${_CCINCFLAGS} ${_CCDEFFLAGS} -c ${SRCLST}'
+cc_fun = Task.compile_fun_noshell('batched_cc', cc_str)[0]
 
-	@feature('cc')
-	@before('apply_core')
-	def add_mmd_cc(self):
-		if self.env.get_flat('CCFLAGS').find('-MD') < 0:
-			self.env.append_value('CCFLAGS', '-MD')
+cc_str = '${CXX} ${CXXFLAGS} ${CPPFLAGS} ${_CXXINCFLAGS} ${_CXXDEFFLAGS} -c ${SRCLST}'
+cxx_fun = Task.compile_fun_noshell('batched_cxx', cc_str)[0]
 
-	@feature('cxx')
-	@before('apply_core')
-	def add_mmd_cxx(self):
-		if self.env.get_flat('CXXFLAGS').find('-MD') < 0:
-			self.env.append_value('CXXFLAGS', '-MD')
-
-	def scan(self):
-		"the scanner does not do anything initially"
-		nodes = self.generator.bld.node_deps.get(self.unique_id(), [])
-		names = [] # self.generator.bld.raw_deps.get(self.unique_id(), [])
-		return (nodes, names)
-
-	for name in 'cc cxx'.split():
-		try:
-			cls = Task.TaskBase.classes[name]
-		except KeyError:
-			pass
-		else:
-			cls.scan = scan
-
-count = 12345
+count = 70000
 class batch_task(Task.Task):
 	color = 'RED'
-	before = 'cc_link cxx_link ar_link_static'
+	before = 'cc_link cxx_link static_link'
 
 	def __str__(self):
 		return '(batch compilation)\n'
 
 	def __init__(self, *k, **kw):
 		Task.Task.__init__(self, *k, **kw)
-		self.slaves=[]
-		self.inputs=[]
-		#self.outputs=[]
+		self.slaves = []
+		self.inputs = []
 		self.hasrun = 0
 
 		global count
@@ -99,135 +73,47 @@ class batch_task(Task.Task):
 		outputs = []
 		self.outputs = []
 
-		self.slaves = [t for t in self.slaves if t.hasrun != SKIPPED]
-
+		srclst = []
+		slaves = []
 		for t in self.slaves:
-			#self.inputs.extend(t.inputs)
-			outputs.extend(t.outputs)
+			if t.hasrun != SKIPPED:
+				slaves.append(t)
+				srclst.append(t.inputs[0].abspath(self.env))
 
-		# unfortunately building the files in batch mode outputs
-		# them into the current folder (the build dir)
-		# move them to the correct location
+		self.env.SRCLST = srclst
+		self.cwd = slaves[0].inputs[0].parent.abspath(self.env)
 
-		lst = []
-		lst2 = []
-		for id in xrange(len(self.slaves)):
-			name = 'batch_%d_%d.c' % (self.idx, id)
-			lst.append(name)
-			f = open(name, 'wb')
-			f.write('#include "%s"\n' % self.slaves[id].inputs[0].relpath_gen(self.generator.bld.bldnode))
-			f.close()
-
-			if USE_SHELL:
-				si = '/bin/mv -f %s %s' % (name.replace('.c', '.o'), self.slaves[id].outputs[0].abspath(self.slaves[id].env))
-				lst2.append(si)
-
-		if USE_SHELL:
-			self.env['CC_TGT_F'] = self.env['CXX_TGT_F'] = '-c ' + " ".join(lst) + " && (%s)" % " && ".join(lst2)
+		if self.slaves[0].__class__.__name__ == 'cc':
+			ret = cc_fun(self)
 		else:
-			self.env['CC_TGT_F'] = self.env['CXX_TGT_F'] = '-c ' + " ".join(lst)
+			ret = cxx_fun(self)
 
-
-		ret = self.slaves[0].__class__.__dict__['oldrun'](self)
 		if ret:
 			return ret
 
-		self.outputs = outputs
-
-		env = self.slaves[0].env
-		rootdir = self.generator.bld.srcnode.abspath(env)
-
-		if not USE_SHELL:
-			for id in xrange(len(self.slaves)):
-				name = 'batch_%d_%d.o' % (self.idx, id)
-				#print "moving", name, self.slaves[id].outputs[0].abspath(self.slaves[id].env)
-
-				task = self.slaves[id]
-				dest = task.outputs[0].abspath(task.env)
-				try:
-					os.unlink(dest)
-				except OSError:
-					pass
-				os.rename(name, dest)
-				#shutil.move(name, task.outputs[0].abspath(task.env))
-
-		if GCCDEPS:
-			for id in xrange(len(self.slaves)):
-				task = self.slaves[id]
-				name = 'batch_%d_%d.d' % (self.idx, id)
-
-				f = open(name, 'r')
-				txt = f.read()
-				f.close()
-				os.unlink(name)
-
-				txt = txt.replace('\\\n', '')
-
-				lst = txt.strip().split(':')
-				val = ":".join(lst[1:])
-				val = val.split()
-
-				# remove the first two sources
-				val = val[2:]
-
-				lock.acquire()
-				nodes = []
-				for x in val:
-					if os.path.isabs(x):
-						node = self.generator.bld.root.find_resource(x)
-					else:
-						x = x.lstrip('../')
-						node = self.generator.bld.srcnode.find_resource(x)
-
-					if not node:
-						raise ValueError, 'could not find' + x
-					else:
-						nodes.append(node)
-				lock.release()
-
-				Logs.debug('deps: real scanner for %s returned %s' % (str(self), str(nodes)))
-
-				id = self.unique_id()
-				self.generator.bld.node_deps[id] = nodes
-				self.generator.bld.raw_deps[id] = []
-
-				try: delattr(self, 'cache_sig')
-				except: pass
-				Task.Task.post_run(task)
-
-		return None
-
-	def post_run(self):
-		for t in self.slaves:
-			sig = t.signature()
-			for node in t.outputs:
-				variant = node.variant(t.env)
-				t.generator.bld.node_sigs[variant][node.id] = sig
-
-			t.generator.bld.task_sigs[t.unique_id()] = t.cache_sig
+		for t in slaves:
+			t.old_post_run()
 
 from TaskGen import extension, feature, after
 
 import cc, cxx
 def wrap(fun):
 	def foo(self, node):
-		task = fun(self, node)
-		if not getattr(self, 'master', None):
-			self.master = self.create_task('batch')
-			try:
-				self.all_masters.append(self.master)
-			except AttributeError:
-				self.all_masters = [self.master]
-		else:
-			if len(self.master.slaves) > MAX_BATCH:
-				# another group
-				self.master = self.create_task('batch')
-				try:
-					self.all_masters.append(self.master)
-				except AttributeError:
-					self.all_masters = [self.master]
+		# we cannot control the extension, this sucks
+		self.obj_ext = '.o'
 
-		self.master.add_slave(task)
+		task = fun(self, node)
+		if not getattr(self, 'masters', None):
+			self.masters = {}
+
+		if not node.parent.id in self.masters:
+			m = self.masters[node.parent.id] = self.master = self.create_task('batch')
+		else:
+			m = self.masters[node.parent.id]
+			if len(m.slaves) > MAX_BATCH:
+				m = self.masters[node.parent.id] = self.master = self.create_task('batch')
+
+		m.add_slave(task)
 		return task
 	return foo
 
@@ -251,22 +137,7 @@ for c in ['cc', 'cxx']:
 
 	setattr(t, 'oldrun', t.__dict__['run'])
 	setattr(t, 'run', run)
+	setattr(t, 'old_post_run', t.post_run)
 	setattr(t, 'post_run', post_run)
 	setattr(t, 'can_retrieve_cache', can_retrieve_cache)
-
-if MAXPARALLEL:
-	# relax the constraints between cxx and cxx_link (in the build section)
-	Task.TaskBase.classes['cxx'].ext_out = []
-	batch_task.before = ''
-
-	@feature('cc', 'cxx')
-	@after('apply_link')
-	def masters(self):
-		try:
-			link_task = self.link_task
-		except AttributeError:
-			pass
-		else:
-			for k in self.all_masters:
-				link_task.set_run_after(k)
 
