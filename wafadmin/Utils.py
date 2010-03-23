f31186789e14b864527ac0754af330f7496ec69b
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-03-23 21:51:03 +0000

    do not immediately fail if md5 is not present
    git-svn-id: http://waf.googlecode.com/svn/trunk@7456 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Utils.py
+++ wafadmin/Utils.py
@@ -130,18 +130,22 @@ try:
 
 except ImportError:
 	try:
-		from hashlib import md5
-	except ImportError:
-		from md5 import md5
+		try:
+			from hashlib import md5
+		except ImportError:
+			from md5 import md5
 
-	def h_file(filename):
-		f = open(filename, 'rb')
-		m = md5()
-		while (filename):
-			filename = f.read(100000)
-			m.update(filename)
-		f.close()
-		return m.digest()
+		def h_file(filename):
+			f = open(filename, 'rb')
+			m = md5()
+			while (filename):
+				filename = f.read(100000)
+				m.update(filename)
+			f.close()
+			return m.digest()
+	except ImportError:
+		# portability fixes may be added elsewhere (although, md5 should be everywhere by now)
+		md5 = None
 
 class ordered_dict(UserDict):
 	def __init__(self, dict = None):
