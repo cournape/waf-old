31fe292782e8b2660fa01bf831c7624f62c35188
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-04-01 00:00:44 +0000

    update
    git-svn-id: http://waf.googlecode.com/svn/trunk@7532 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- playground/crazy_rpath/foo.c
+++ playground/crazy_rpath/foo.c
@@ -1,3 +1,4 @@
 int bang() {
 	return 0;
 }
+
--- playground/crazy_rpath/morelink.py
+++ playground/crazy_rpath/morelink.py
@@ -12,58 +12,49 @@ cc = Task.TaskBase.classes['cc_link']
 class inst_cc(cc):
 	"""identical to the link task except that it only runs at install time"""
 	def runnable_status(self):
-		if not self.generator.bld.is_install:
-			return Constants.SKIP_ME
+		#if not self.generator.bld.is_install:
+		#	return Constants.SKIP_ME
 		return Task.Task.runnable_status(self)
 
 old = TaskGen.task_gen.apply_link
 
 @feature('cprogram', 'cshlib', 'cstaticlib')
-@after('apply_core')
-@before('default_link_install')
-def apply_link(self):
-	"""replace the method apply_link"""
-	link = getattr(self, 'link', None)
+@after('apply_link')
+@before('default_link_install', 'apply_vnum')
+def no_rpath(self):
 
-	if link and link != 'cc_link':
-		return old(self)
-
-	rpath = get_target_name(self)
-	target = rpath.replace('.so', '_.so')
-
+	name = get_target_name(self).replace('.so', '__.so')
 	tsk = self.create_task('inst_cc')
-	outputs = [t.outputs[0] for t in self.compiled_tasks]
-	tsk.set_inputs(outputs)
-	tsk.set_outputs(self.path.find_or_declare(target))
+	tsk.inputs = self.link_task.outputs
+	tsk.set_outputs(self.path.find_or_declare(name))
 
-	rp = self.create_task('cc_link')
-	rp.inputs = tsk.inputs
-	rp.set_outputs(self.path.find_or_declare(rpath))
-	tsk.set_run_after(rp) # to link we need the .so files present
-	rp.env = tsk.env.copy()
+	tsk.set_run_after(self.link_task)
+	tsk.env = self.link_task.env
+	self.link_task.env = self.link_task.env.copy()
 
-	# wrong names, we know
+	env = self.link_task.env
+
 	self.rpath_task = tsk
-	self.link_task = rp
 
 	if not getattr(self, 'vnum', None):
-		if self.install_path:
-			self.bld.install_as(self.install_path + '/' + rpath, tsk.outputs[0], env=self.env, chmod=0755)
+	#	if self.install_path:
+	#		self.bld.install_as(self.install_path + '/' + rpath, tsk.outputs[0], env=self.env, chmod=0755)
 		self.meths.remove('default_link_install')
 
+
 @feature('cprogram', 'cshlib', 'cstaticlib')
-@after('apply_link')
+@after('apply_lib_vars', 'apply_link', 'no_rpath')
 @before('apply_obj_vars')
 def evil_rpath(self):
-	"""disable normal rpath processing"""
-	rp = self.rpath_task
 
-	# rpath flag mess
-	rpath_st = rp.env['RPATH_ST']
-	app = rp.env.append_unique
-	for i in rp.env['RPATH']:
+	env = self.env
+	rpath_st = env['RPATH_ST']
+	app = self.link_task.env.append_unique
+	for i in self.env['RPATH']:
 		if i and rpath_st:
 			app('LINKFLAGS', rpath_st % i)
 
 	self.env['RPATH'] = []
+
+
 
