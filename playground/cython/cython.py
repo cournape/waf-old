13771618cd67accce9c02d2732285587f247e334
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-04-27 20:25:50 +0000

    #668
    git-svn-id: http://waf.googlecode.com/svn/trunk@7743 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- playground/cython/cython.py
+++ playground/cython/cython.py
@@ -4,10 +4,34 @@
 
 "Cython"
 
+import re
 import TaskGen
 
+re_cyt = re.compile('import\\s(\\w+)\\s*$', re.M)
+
 def cscan(self):
-	return ((), ())
+
+	txt = self.inputs[0].read(self.env)
+	mods = []
+	for m in re_cyt.finditer(txt):
+		mods.append(m.group(1))
+
+	incs = getattr(self.generator, 'cython_includes', [])
+	incs = [self.generator.path.find_dir(x) for x in incs]
+	incs.append(self.inputs[0].parent)
+
+	found = []
+	missing = []
+	for x in mods:
+		for y in incs:
+			k = y.find_resource(x + '.pxd')
+			if k:
+				found.append(k)
+				break
+		else:
+			missing.append(x)
+
+	return (found, missing)
 
 TaskGen.declare_chain(name='cython', rule='${CYTHON} ${CYTHONFLAGS} ${SRC} -o ${TGT}', ext_in=['.pyx'], ext_out=['.c'], scan=cscan)
 
