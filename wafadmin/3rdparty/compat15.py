35a2c7021e73a5e9af6e407d6cf38182df8cde1f
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-05-10 20:19:31 +0000

    More compatibility stuff
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@7883 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/3rdparty/compat15.py
+++ wafadmin/3rdparty/compat15.py
@@ -15,20 +15,20 @@ def old_importpaths(self):
 	if getattr(self, 'importpaths', []):
 		self.includes = self.importpaths
 
-"""
 import Base
-old = Base.load_tool
+eld = Base.load_tool
 def load_tool(*k, **kw):
-	ret = old(*k, **kw)
+	ret = eld(*k, **kw)
+	return ret
 	if 'options' in ret.__dict__:
 		ret.set_options = ret.options
 Base.load_tool = load_tool
-"""
 
-import Scripting, Base
+import Scripting
 old = Scripting.set_main_module
 def set_main_module(f):
 	old(f)
-	if not 'set_options' in Base.g_module.__dict__:
-		Base.g_module.set_options = Base.g_module.options
+	if 'set_options' in Base.g_module.__dict__:
+		Base.g_module.options = Base.g_module.set_options
 Scripting.set_main_module = set_main_module
+
