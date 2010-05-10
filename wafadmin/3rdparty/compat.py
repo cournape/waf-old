3db70a1bfa3c8cfa414ced3fb8fef8d7c0a8d35e
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-05-10 18:55:54 +0000

    compatibility for import paths
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@7875 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/3rdparty/compat.py
+++ wafadmin/3rdparty/compat.py
@@ -7,4 +7,11 @@ import Configure
 Configure.ConfigurationContext.sub_config = Configure.ConfigurationContext.recurse
 Configure.conftest = Configure.conf
 
+from TaskGen import before, feature
+
+@feature('d')
+@before('apply_incpaths')
+def old_importpaths(self):
+	if getattr(self, 'importpaths', []):
+		self.includes = self.importpaths
 
