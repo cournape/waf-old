f9274fe1486fa10e2e29694f368d63f358addec7
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-06-01 19:56:56 +0000

    fix ant_glob for recursive matching
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@8084 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Node.py
+++ wafadmin/Node.py
@@ -534,7 +534,7 @@ class Node(object):
 				if getattr(node, 'cache_isdir', None) or os.path.isdir(node.abspath()):
 					node.cache_isdir = True
 					if maxdepth:
-						for k in node.ant_iter(maxdepth=maxdepth - 1, pats=npats):
+						for k in node.ant_iter(accept=accept, maxdepth=maxdepth - 1, pats=npats):
 							yield k
 		raise StopIteration
 
