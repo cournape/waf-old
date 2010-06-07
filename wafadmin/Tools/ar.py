c91c92ea912d682b1c5562e4819aa26c01192d97
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-06-07 22:05:35 +0000

    apis
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@8157 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Tools/ar.py
+++ wafadmin/Tools/ar.py
@@ -10,15 +10,17 @@ from wafadmin import Task, Utils
 from wafadmin.Configure import conf
 
 ar_str = '${AR} ${ARFLAGS} ${AR_TGT_F}${TGT} ${AR_SRC_F}${SRC}'
-cls = Task.simple_task_type('static_link', ar_str, color='YELLOW', ext_in='.o')
-
-# remove the output in case it already exists
-old = cls.run
-def wrap(self):
-	try: os.remove(self.outputs[0].abspath())
-	except OSError: pass
-	return old(self)
-setattr(cls, 'run', wrap)
+class static_link(Task.Task):
+	color = 'YELLOW'
+	run_str = ar_str
+	ext_in = ['.o']
+	def run(self):
+		"""remove the output in case it already exists"""
+		try:
+			os.remove(self.outputs[0].abspath())
+		except OSError:
+			pass
+		return Task.Task.run(self)
 
 @conf
 def find_ar(conf):
