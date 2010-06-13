5e527efa871ffd3c1cbbde30371518e4592ca7c5
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-06-13 18:17:50 +0000

    this syntax looks better
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@8242 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- waflib/Tools/cxx.py
+++ waflib/Tools/cxx.py
@@ -14,10 +14,15 @@ def cxx_hook(self, node):
 if not '.c' in TaskGen.task_gen.mappings:
 	TaskGen.task_gen.mappings['.c'] = TaskGen.task_gen.mappings['.cpp']
 
-cxx_str = '${CXX} ${CXXFLAGS} ${CPPFLAGS} ${_INCFLAGS} ${_DEFFLAGS} ${CXX_SRC_F}${SRC} ${CXX_TGT_F}${TGT}'
-cxx = Task.task_factory('cxx', cxx_str, color='GREEN', ext_in=['.h'], scan=ccroot.scan)
-cxx.vars.append('CXXDEPS')
+class cxx(Task.Task):
+	color   = 'GREEN'
+	run_str = '${CXX} ${CXXFLAGS} ${CPPFLAGS} ${_INCFLAGS} ${_DEFFLAGS} ${CXX_SRC_F}${SRC} ${CXX_TGT_F}${TGT}'
+	vars    = ['CXXDEPS']
+	ext_in  = ['.h']
+	scan    = ccroot.scan
 
-link_str = '${LINK_CXX} ${CXXLNK_SRC_F}${SRC} ${CXXLNK_TGT_F}${TGT[0].abspath()} ${LINKFLAGS}'
-cxx_link = Task.task_factory('cxx_link', link_str, color='YELLOW', ext_out='.bin')
+class cxx_link(Task.Task):
+	color   = 'YELLOW'
+	run_str = '${LINK_CXX} ${CXXLNK_SRC_F}${SRC} ${CXXLNK_TGT_F}${TGT[0].abspath()} ${LINKFLAGS}'
+	ext_out = ['.bin']
 
