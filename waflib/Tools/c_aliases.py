d9093732c16ba50ac9cfcb759efc4831f3e64cae
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-06-13 00:10:21 +0000

    missing file
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@8216 f0382ac9-c320-0410-b3f0-b508d59f5a85

---
+++ waflib/Tools/c_aliases.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Thomas Nagy, 2005-2010 (ita)
+
+"base for all c/c++ programs and libraries"
+
+import os, sys, re, subprocess
+from wafadmin import TaskGen, Task, Utils, Logs, Build, Options, Node, Errors
+from wafadmin.Logs import error, debug, warn
+from wafadmin.Utils import md5
+from wafadmin.TaskGen import after, before, feature, taskgen_method
+from wafadmin.Configure import conf
+from wafadmin.Tools import preproc
+
+def sniff_features(**kw):
+	"""look at the source files and return the features (mainly cc and cxx)"""
+	has_c = False
+	has_cxx = False
+	s = Utils.to_list(kw['source'])
+	for name in s:
+		if name.endswith('.c'):
+			has_c = True
+		elif name.endswith('.cxx') or name.endswith('.cpp') or name.endswith('.c++'):
+			has_cxx = True
+	lst = []
+	if has_c:
+		lst.append('cc')
+	if has_cxx:
+		lst.append('cxx')
+	return lst
+
+def program(bld, *k, **kw):
+	"""alias for features='cc cprogram' bound to the build context"""
+	if not 'features' in kw:
+		kw['features'] = ['cprogram'] + sniff_features(**kw)
+	return bld(*k, **kw)
+Build.BuildContext.program = program
+
+def shlib(bld, *k, **kw):
+	"""alias for features='cc cshlib' bound to the build context"""
+	if not 'features' in kw:
+		kw['features'] = ['cshlib'] + sniff_features(**kw)
+	return bld(*k, **kw)
+Build.BuildContext.shlib = shlib
+
+def stlib(bld, *k, **kw):
+	"""alias for features='cc cstlib' bound to the build context"""
+	if not 'features' in kw:
+		kw['features'] = ['cstlib'] + sniff_features(**kw)
+	return bld(*k, **kw)
+Build.BuildContext.stlib = stlib
+
