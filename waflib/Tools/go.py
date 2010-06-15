7c6a1d08e0bde0a9e59fd681a1f2491587312a7c
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-06-15 20:42:29 +0000

    apis
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@8302 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- waflib/Tools/go.py
+++ waflib/Tools/go.py
@@ -18,6 +18,7 @@ class gopackage(static_link):
 
 class goprogram(link_task):
 	run_str = '${GOL} ${GOLFLAGS} -o ${TGT} ${SRC}'
+	inst_to = '${BINDIR}'
 
 
 def configure(conf):
@@ -31,13 +32,11 @@ def configure(conf):
 	if conf.env.GO_PLATFORM == 'x86_64':
 		set_def('GO_COMPILER', '6g')
 		set_def('GO_LINKER', '6l')
-		set_def('GO_EXTENSION', '.6')
 	elif conf.env.GO_PLATFORM == 'i386':
 		set_def('GO_COMPILER', '8g')
 		set_def('GO_LINKER', '8l')
-		set_def('GO_EXTENSION', '.8')
 
-	if not (conf.env.GO_COMPILER or conf.env.GO_LINKER or conf.env.GO_EXTENSION):
+	if not (conf.env.GO_COMPILER or conf.env.GO_LINKER):
 		raise conf.fatal('Unsupported platform ' + platform.machine())
 
 	set_def('GO_PACK', 'gopack')
@@ -51,8 +50,7 @@ def configure(conf):
 def compile_go(self, node):
 	return self.create_compiled_task('go', node)
 
-
-@feature('go')
+@feature('gopackage', 'goprogram')
 @after('process_source', 'apply_incpaths')
 def go_local_libs(self):
 	names = self.to_list(getattr(self, 'uselib_local', []))
@@ -69,36 +67,6 @@ def go_local_libs(self):
 		self.env.append_unique('GOLFLAGS', ['-L%s' % path])
 
 """
-@feature('go')
-@after('apply_core')
-def apply_compile_go(self):
-	try:
-		nodes = self.go_nodes
-	except AttributeError:
-		self.go_compile_task = None
-	else:
-		self.go_compile_task = self.create_task('gocompile',
-			nodes,
-			[self.path.find_or_declare(self.target + self.env.GO_EXTENSION)])
-
-@feature('gopackage', 'goprogram')
-@after('apply_compile_go')
-def apply_goinc(self):
-	if not getattr(self, 'go_compile_task', None):
-		return
-
-	names = self.to_list(getattr(self, 'uselib_local', []))
-	for name in names:
-		obj = self.name_to_obj(name)
-		if not obj:
-			raise Utils.WafError('object %r was not found in uselib_local '
-					'(required by %r)' % (lib_name, self.name))
-		obj.post()
-		self.go_compile_task.set_run_after(obj.go_package_task)
-		self.go_compile_task.deps_nodes.extend(obj.go_package_task.outputs)
-		self.env.append_unique('GOCFLAGS', ['-I' + obj.path.abspath(obj.env)])
-		self.env.append_unique('GOLFLAGS', ['-L' + obj.path.abspath(obj.env)])
-
 @feature('gopackage')
 @after('apply_goinc')
 def apply_gopackage(self):
