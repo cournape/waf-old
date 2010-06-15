e6cd26314b3ce97ce6a70af2b75f93356064a368
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-06-15 20:49:25 +0000

    cleanup
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@8303 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- waflib/Tools/go.py
+++ waflib/Tools/go.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 # encoding: utf-8
-# go.py - Waf tool for the Go programming language
-# By: Tom Wambold <tom5760@gmail.com>
+# Tom Wambold tom5760 gmail.com 2009
+# Thomas Nagy 2010
+
+"""
+go language support
+
+The methods apply_link and apply_incpaths from ccroot.py are re-used
+"""
 
 import platform
 
 import Utils, Task
 from TaskGen import feature, extension, after
-
 from waflib.Tools.ccroot import link_task, static_link
 
 class go(Task.Task):
@@ -20,6 +25,26 @@ class goprogram(link_task):
 	run_str = '${GOL} ${GOLFLAGS} -o ${TGT} ${SRC}'
 	inst_to = '${BINDIR}'
 
+@extension('.go')
+def compile_go(self, node):
+	return self.create_compiled_task('go', node)
+
+@feature('gopackage', 'goprogram')
+@after('process_source', 'apply_incpaths')
+def go_local_libs(self):
+	names = self.to_list(getattr(self, 'uselib_local', []))
+	for name in names:
+		tg = self.bld.name_to_obj(name)
+		if not tg:
+			raise Utils.WafError('no target of name %r necessary for %r in go uselib local' % (name, self))
+		tg.post()
+		for tsk in self.tasks:
+			if isinstance(tsk, go):
+				tsk.set_run_after(tg.link_task)
+				tsk.deps_nodes.extend(tg.link_task.outputs)
+		path = tg.link_task.outputs[0].parent.abspath()
+		self.env.append_unique('GOCFLAGS', ['-I%s' % path])
+		self.env.append_unique('GOLFLAGS', ['-L%s' % path])
 
 def configure(conf):
 
@@ -45,44 +70,4 @@ def configure(conf):
 	conf.find_program(conf.env.GO_COMPILER, var='GOC')
 	conf.find_program(conf.env.GO_LINKER,   var='GOL')
 	conf.find_program(conf.env.GO_PACK,     var='GOP')
-
-@extension('.go')
-def compile_go(self, node):
-	return self.create_compiled_task('go', node)
-
-@feature('gopackage', 'goprogram')
-@after('process_source', 'apply_incpaths')
-def go_local_libs(self):
-	names = self.to_list(getattr(self, 'uselib_local', []))
-	for name in names:
-		obj = self.bld.name_to_obj(name)
-		if not obj:
-			raise Utils.WafError('no target of name %r necessary for %r in go uselib local' % (name, self))
-		obj.post()
-		for tsk in self.tasks:
-			tsk.set_run_after(obj.link_task)
-			tsk.deps_nodes.extend(obj.link_task.outputs)
-		path = obj.link_task.outputs[0].parent.abspath()
-		self.env.append_unique('GOCFLAGS', ['-I%s' % path])
-		self.env.append_unique('GOLFLAGS', ['-L%s' % path])
-
-"""
-@feature('gopackage')
-@after('apply_goinc')
-def apply_gopackage(self):
-	self.go_package_task = self.create_task('gopack',
-			self.go_compile_task.outputs[0],
-			self.path.find_or_declare(self.target + self.env.GO_PACK_EXTENSION))
-	self.go_package_task.set_run_after(self.go_compile_task)
-	self.go_package_task.deps_nodes.extend(self.go_compile_task.outputs)
-
-@feature('goprogram')
-@after('apply_goinc')
-def apply_golink(self):
-	self.go_link_task = self.create_task('golink',
-			self.go_compile_task.outputs[0],
-			self.path.find_or_declare(self.target))
-	self.go_link_task.set_run_after(self.go_compile_task)
-	self.go_link_task.deps_nodes.extend(self.go_compile_task.outputs)
-"""
 
