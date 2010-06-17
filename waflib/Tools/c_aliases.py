8054e5f9ab4e9d0ce99e6a798e861e18f31043f5
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-06-17 17:48:14 +0000

    more code
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@8352 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- waflib/Tools/c_aliases.py
+++ waflib/Tools/c_aliases.py
@@ -7,41 +7,74 @@
 import os, sys, re, subprocess
 from waflib import Utils, Build
 
+def get_extensions(lst):
+	ret = []
+	for x in Utils.to_list(lst):
+		try:
+			ret.append(x[x.rfind('.') + 1:])
+		except:
+			pass
+	return ret
+
 def sniff_features(**kw):
 	"""look at the source files and return the features (mainly cc and cxx)"""
-	has_c = False
-	has_cxx = False
-	s = Utils.to_list(kw['source'])
-	for name in s:
-		if name.endswith('.c'):
-			has_c = True
-		elif name.endswith('.cxx') or name.endswith('.cpp') or name.endswith('.c++'):
-			has_cxx = True
-	lst = []
-	if has_c:
-		lst.append('cc')
-	if has_cxx:
-		lst.append('cxx')
-	return lst
+	exts = get_extensions(kw['source'])
+	type = kw['_type']
+	print exts
+
+	if 'cxx' in exts or 'cpp' in exts or 'c++' in exts:
+		if '_type' == 'program':
+			return 'cxx cxxprogram'
+		if '_type' == 'shlib':
+			return 'cxx cxxshlib'
+		if '_type' == 'stlib':
+			return 'cxx cxxstlib'
+		return 'cxx'
+
+	if 'd' in exts:
+		if '_type' == 'program':
+			return 'd dprogram'
+		if '_type' == 'shlib':
+			return 'd dshlib'
+		if '_type' == 'stlib':
+			return 'd dstlib'
+		return 'd'
+
+	if 'vala' in exts or 'c' in exts:
+		if '_type' == 'program':
+			return 'c cprogram'
+		if '_type' == 'shlib':
+			return 'c cshlib'
+		if '_type' == 'stlib':
+			return 'c cstlib'
+		return 'cc'
+
+	if 'java' in exts:
+		return 'java'
+
+	return ''
 
 def program(bld, *k, **kw):
 	"""alias for features='cc cprogram' bound to the build context"""
 	if not 'features' in kw:
-		kw['features'] = ['cprogram'] + sniff_features(**kw)
+		kw['_type'] = 'program'
+		kw['features'] = sniff_features(**kw)
 	return bld(*k, **kw)
 Build.BuildContext.program = program
 
 def shlib(bld, *k, **kw):
 	"""alias for features='cc cshlib' bound to the build context"""
 	if not 'features' in kw:
-		kw['features'] = ['cshlib'] + sniff_features(**kw)
+		kw['_type'] = 'shlib'
+		kw['features'] = sniff_features(**kw)
 	return bld(*k, **kw)
 Build.BuildContext.shlib = shlib
 
 def stlib(bld, *k, **kw):
 	"""alias for features='cc cstlib' bound to the build context"""
 	if not 'features' in kw:
-		kw['features'] = ['cstlib'] + sniff_features(**kw)
+		kw['_type'] = 'stlib'
+		kw['features'] = sniff_features(**kw)
 	return bld(*k, **kw)
 Build.BuildContext.stlib = stlib
 
