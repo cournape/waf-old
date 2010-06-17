f3c643114fe9fbdbe5d1938f406419624ce8b156
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-06-17 18:10:43 +0000

    cleanup
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@8358 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- waflib/Tools/perl.py
+++ waflib/Tools/perl.py
@@ -5,7 +5,7 @@
 import os, subprocess
 from waflib import Task, Options, Utils
 from waflib.Configure import conf
-from waflib.TaskGen import extension, taskgen, feature, before
+from waflib.TaskGen import extension, feature, before
 
 @before('apply_incpaths', 'apply_lib_vars')
 @feature('perlext')
@@ -27,7 +27,7 @@ class xsubpp(Task.Task):
 	ext_out = '.h'
 
 @conf
-def check_perl_version(conf, minver=None):
+def check_perl_version(self, minver=None):
 	"""
 	Checks if perl is installed.
 
@@ -39,12 +39,11 @@ def check_perl_version(conf, minver=None):
 	res = True
 
 	if not getattr(Options.options, 'perlbinary', None):
-		perl = conf.find_program("perl", var="PERL")
+		perl = self.find_program('perl', var='PERL')
 		if not perl:
 			return False
 	else:
-		perl = Options.options.perlbinary
-		conf.env['PERL'] = perl
+		self.env['PERL'] = perl = Options.options.perlbinary
 
 	version = Utils.cmd_output(perl + " -e'printf \"%vd\", $^V'")
 	if not version:
@@ -59,26 +58,26 @@ def check_perl_version(conf, minver=None):
 		cver = ""
 	else:
 		cver = ".".join(map(str,minver))
-	conf.check_message("perl", cver, res, version)
+	self.msg('perl', cver)
 	return res
 
 @conf
-def check_perl_module(conf, module):
+def check_perl_module(self, module):
 	"""
 	Check if specified perlmodule is installed.
 
 	Minimum version can be specified by specifying it after modulename
 	like this:
 
-	conf.check_perl_module("Some::Module 2.92")
+	self.check_perl_module("Some::Module 2.92")
 	"""
-	cmd = [conf.env['PERL'], '-e', 'use %s' % module]
+	cmd = [self.env['PERL'], '-e', 'use %s' % module]
 	r = subprocess.call(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
-	conf.check_message("perl module %s" % module, "", r)
+	self.msg('perl module %s' % module, r)
 	return r
 
 @conf
-def check_perl_ext_devel(conf):
+def check_perl_ext_devel(self):
 	"""
 	Check for configuration needed to build perl extensions.
 
@@ -88,28 +87,29 @@ def check_perl_ext_devel(conf):
 	which can be overridden by --with-perl-archdir option.
 	"""
 
-	perl = conf.env.PERL
+	env = self.env
+	perl = env.PERL
 	if not perl:
-		conf.fatal('find perl first')
+		self.fatal('find perl first')
 
 	def read_out(cmd):
 		return Utils.to_list(Utils.cmd_output(perl + cmd))
 
-	conf.env["LINKFLAGS_PERLEXT"] = read_out(" -MConfig -e'print $Config{lddlflags}'")
-	conf.env["CPPPATH_PERLEXT"] = read_out(" -MConfig -e'print \"$Config{archlib}/CORE\"'")
-	conf.env["CCFLAGS_PERLEXT"] = read_out(" -MConfig -e'print \"$Config{ccflags} $Config{cccdlflags}\"'")
+	env["LINKFLAGS_PERLEXT"] = read_out(" -MConfig -e'print $Config{lddlflags}'")
+	env["CPPPATH_PERLEXT"] = read_out(" -MConfig -e'print \"$Config{archlib}/CORE\"'")
+	env["CCFLAGS_PERLEXT"] = read_out(" -MConfig -e'print \"$Config{ccflags} $Config{cccdlflags}\"'")
 
-	conf.env["XSUBPP"] = read_out(" -MConfig -e'print \"$Config{privlib}/ExtUtils/xsubpp$Config{exe_ext}\"'")
-	conf.env["EXTUTILS_TYPEMAP"] = read_out(" -MConfig -e'print \"$Config{privlib}/ExtUtils/typemap\"'")
+	env["XSUBPP"] = read_out(" -MConfig -e'print \"$Config{privlib}/ExtUtils/xsubpp$Config{exe_ext}\"'")
+	env["EXTUTILS_TYPEMAP"] = read_out(" -MConfig -e'print \"$Config{privlib}/ExtUtils/typemap\"'")
 
 	if not getattr(Options.options, 'perlarchdir', None):
-		conf.env["ARCHDIR_PERL"] = Utils.cmd_output(perl + " -MConfig -e'print $Config{sitearch}'")
+		env['ARCHDIR_PERL'] = Utils.cmd_output(perl + " -MConfig -e'print $Config{sitearch}'")
 	else:
-		conf.env["ARCHDIR_PERL"] = getattr(Options.options, 'perlarchdir')
+		env['ARCHDIR_PERL'] = getattr(Options.options, 'perlarchdir')
 
-	conf.env['perlext_PATTERN'] = '%s.' + Utils.cmd_output(perl + " -MConfig -e'print $Config{dlext}'")
+	env['perlext_PATTERN'] = '%s.' + Utils.cmd_output(perl + " -MConfig -e'print $Config{dlext}'")
 
 def options(opt):
-	opt.add_option("--with-perl-binary", type="string", dest="perlbinary", help = 'Specify alternate perl binary', default=None)
-	opt.add_option("--with-perl-archdir", type="string", dest="perlarchdir", help = 'Specify directory where to install arch specific files', default=None)
+	opt.add_option('--with-perl-binary', type='string', dest='perlbinary', help = 'Specify alternate perl binary', default=None)
+	opt.add_option('--with-perl-archdir', type='string', dest='perlarchdir', help = 'Specify directory where to install arch specific files', default=None)
 
