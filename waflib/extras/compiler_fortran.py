4e3a955b1d38a27d98238ff212f423a9eee16f08
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-06-19 19:25:20 +0000

    update
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@8441 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- waflib/extras/compiler_fortran.py
+++ waflib/extras/compiler_fortran.py
@@ -1,50 +1,57 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import os, sys, imp, types, ccroot
+import os, sys, imp, types
 from waflib import Utils, Configure, Options
+from waflib.extras import fortran
 
-fortran_compiler = {
-	'darwin': ['gfortran', 'ifort'],
-	'linux': ['gfortran', 'ifort'],
+fc_compiler = {
+	'darwin' : ['gfortran', 'ifort'],
+	'linux'  : ['gfortran', 'ifort'],
 	'default': ['gfortran']
 }
 
 def __list_possible_compiler(platform):
 	try:
-		return(fortran_compiler[platform])
+		return fc_compiler[platform]
 	except KeyError:
-		return(fortran_compiler["default"])
+		return fc_compiler["default"]
 
-def detect(conf):
+def configure(conf):
 	try:
-		test_for_compiler = Options.options.check_fortran_compiler
+		test_for_compiler = Options.options.check_fc
 	except AttributeError:
-		raise Configure.ConfigurationError(
-				"Add set_options(opt): opt.tool_options('compiler_fortran')")
+		raise Configure.ConfigurationError("Add set_options(opt): opt.tool_options('compiler_fortran')")
+	orig = conf.env
 	for compiler in test_for_compiler.split():
 		try:
+			conf.start_msg('Checking for %r (fortran compiler)' % compiler)
+			conf.env = orig.derive()
 			conf.check_tool(compiler)
-		except Configure.ConfigurationError:
-			pass
+		except conf.errors.ConfigurationError as e:
+			conf.end_msg(False)
+			debug('compiler_fortran: %r' % e)
 		else:
 			if conf.env['FC']:
-				conf.check_message("%s" % compiler, '', True)
-				conf.env["COMPILER_FC"] = "%s" % compiler
-				return
-			conf.check_message("%s" %fortran_compiler, '', False)
-			break
-	conf.env["COMPILER_FC"] = None
+				orig.table = conf.env.get_merged_dict()
+				conf.env = orig
+				conf.end_msg(True)
+				conf.env.COMPILER_FORTRAN = compiler
+				break
+			conf.end_msg(False)
+	else:
+		conf.fatal('could not configure a fortran compiler!')
 
-def set_options(opt):
+def options(opt):
+	build_platform = Utils.unversioned_sys_platform()
 	detected_platform = Options.platform
 	possible_compiler_list = __list_possible_compiler(detected_platform)
-	test_for_compiler = str(" ").join(possible_compiler_list)
+	test_for_compiler = ' '.join(possible_compiler_list)
 	fortran_compiler_opts = opt.add_option_group("Fortran Compiler Options")
 	fortran_compiler_opts.add_option('--check-fortran-compiler',
 			default="%s" % test_for_compiler,
 			help='On this platform (%s) the following Fortran Compiler will be checked by default: "%s"' % (detected_platform, test_for_compiler),
-		dest="check_fortran_compiler")
+		dest="check_fc")
 
 	for compiler in test_for_compiler.split():
 		opt.tool_options('%s' % compiler, option_group=fortran_compiler_opts)
