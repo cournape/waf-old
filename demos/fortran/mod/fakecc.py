e00a39b60dafd8007a521206c877138d040b2a77
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-06-20 16:29:27 +0000

    docs
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@8492 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- demos/fortran/mod/fakecc.py
+++ demos/fortran/mod/fakecc.py
@@ -7,6 +7,7 @@ import re
 
 from waflib import Utils, Task, TaskGen, Logs
 from TaskGen import feature, before, after, extension
+from waflib.Tools import ccroot
 
 IS_MODULE_R = re.compile('module ([a-z]*)')
 USE_MODULE_R = re.compile('use ([a-z]*)')
@@ -15,65 +16,45 @@ USE_MODULE_R = re.compile('use ([a-z]*)')
 def hook(self, node):
 	self.create_compiled_task('fakecc', node)
 
-def ismodule(file):
-    deps = []
-    a = open(file, 'r')
-    try:
-        for l in a.readlines():
-            m = IS_MODULE_R.match(l)
-            if m:
-                deps.append(m.group(1) + '.mod')
-    finally:
-        a.close()
+def ismodule(node):
+	deps = []
+	for l in node.read().splitlines():
+		m = IS_MODULE_R.match(l)
+		if m:
+			deps.append(m.group(1) + '.mod')
+	return deps
 
-    return deps
+def usemodule(node):
+	deps = []
+	for l in node.read().splitlines():
+		m = USE_MODULE_R.match(l)
+		if m:
+			deps.append(m.group(1) + '.mod')
+	return deps
 
-def usemodule(file):
-    deps = []
-    a = open(file, 'r')
-    try:
-        for l in a.readlines():
-            m = USE_MODULE_R.match(l)
-            if m:
-                deps.append(m.group(1) + '.mod')
-    finally:
-        a.close()
-
-    return deps
-
-def compile(src, tgt):
-    t = open(tgt, 'w')
-    try:
-        t.write('compiled')
-    finally:
-        t.close()
-
-    m = ismodule(src)
-    if m:
-	print "%s declares module %s" % (src, m[0])
-        t2 = open(m[0], 'w')
-        try:
-            t2.write('module compiled')
-        finally:
-            t2.close()
+def compile(tsk):
+	tsk.outputs[0].write('compiled')
+	m = ismodule(tsk.inputs[0])
+	if m:
+		print("%s declares module %s" % (tsk.inputs[0], m[0]))
+		t2 = open(m[0], 'w')
+		try:
+			t2.write('module compiled')
+		finally:
+			t2.close()
 
 class fakecc(Task.Task):
 	color = 'YELLOW'
-	def yo(task):
-		env = task.env
+	def run(self):
 		cmd = []
-		if not len(task.outputs) == len(task.inputs) == 1:
+		if not len(self.outputs) == len(self.inputs) == 1:
 			pass
 
-		src = task.inputs[0].srcpath(env)
-		tgt = task.outputs[0].bldpath(env)
-
-		bnodes = task.outputs
-		m = usemodule(src)
+		bnodes = self.outputs
+		m = usemodule(self.inputs[0])
 		if m:
-			print "%s requires module %s" % (src, m[0])
-			print task.inputs[0].parent.exclusive_build_node(m[0])
-			#bnodes.append(task.generator.bld.bldnode.exclusive_build_node(m[0]))
+			print "%s requires module %s" % (self.inputs[0].abspath(), m[0])
+			#bnodes.append(self.generator.bld.bldnode.exclusive_build_node(m[0]))
 
-		compile(src, tgt)
+		compile(self)
 
