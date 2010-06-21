e16429d112fb09d249da0ebb8d80fe2e3cde3068
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-06-21 22:13:16 +0000

    update
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@8499 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- waflib/extras/swig.py
+++ waflib/extras/swig.py
@@ -4,55 +4,18 @@
 # Thomas Nagy 2008-2010 (ita)
 
 import re
-import Task, Utils, Logs
-from TaskGen import extension, taskgen, feature, after
-from Configure import conf
-import c_preproc
+from waflib import Task, Utils, Logs
+from waflib.TaskGen import extension, feature, after
+from waflib.Configure import conf
+from waflib.Tools import c_preproc
 
 """
-Welcome in the hell of adding tasks dynamically
-
-swig interface files may be created at runtime, the module name may be unknown in advance
-
-rev 5859 is much more simple
+tasks have to be added dynamically:
+- swig interface files may be created at runtime
+- the module name may be unknown in advance
 """
 
 SWIG_EXTS = ['.swig', '.i']
-
-swig_str = '${SWIG} ${SWIGFLAGS} ${SRC}'
-cls = Task.task_factory('swig', swig_str, color='BLUE', ext_out='.o .c .cxx')
-
-def runnable_status(self):
-	for t in self.run_after:
-		if not t.hasrun:
-			return ASK_LATER
-
-	if not getattr(self, 'init_outputs', None):
-		self.init_outputs = True
-		if not getattr(self, 'module', None):
-			# search the module name
-			txt = self.inputs[0].read(self.env)
-			m = re_module.search(txt)
-			if not m:
-				raise ValueError("could not find the swig module name")
-			self.module = m.group(1)
-
-		swig_c(self)
-
-		# add the language-specific output files as nodes
-		# call funs in the dict swig_langs
-		for x in self.env['SWIGFLAGS']:
-			# obtain the language
-			x = x[1:]
-			try:
-				fun = swig_langs[x]
-			except KeyError:
-				pass
-			else:
-				fun(self)
-
-	return Task.Task.runnable_status(self)
-setattr(cls, 'runnable_status', runnable_status)
 
 re_module = re.compile('%module(?:\s*\(.*\))?\s+(.+)', re.M)
 
@@ -60,43 +23,75 @@ re_1 = re.compile(r'^%module.*?\s+([\w]+)\s*?$', re.M)
 re_2 = re.compile('%include "(.*)"', re.M)
 re_3 = re.compile('#include "(.*)"', re.M)
 
-def scan(self):
-	"scan for swig dependencies, climb the .i files"
-	env = self.env
+class swig(Task.Task):
+	color   = 'BLUE'
+	run_str = '${SWIG} ${SWIGFLAGS} ${SRC}'
+	ext_out = ['.h'] # might produce .h files although it is not mandatory
 
-	lst_src = []
+	def runnable_status(self):
+		for t in self.run_after:
+			if not t.hasrun:
+				return Task.ASK_LATER
 
-	seen = []
-	to_see = [self.inputs[0]]
+		if not getattr(self, 'init_outputs', None):
+			self.init_outputs = True
+			if not getattr(self, 'module', None):
+				# search the module name
+				txt = self.inputs[0].read()
+				m = re_module.search(txt)
+				if not m:
+					raise ValueError("could not find the swig module name")
+				self.module = m.group(1)
 
-	while to_see:
-		node = to_see.pop(0)
-		if node.id in seen:
-			continue
-		seen.append(node.id)
-		lst_src.append(node)
+			swig_c(self)
 
-		# read the file
-		code = node.read(env)
-		code = c_preproc.re_nl.sub('', code)
-		code = c_preproc.re_cpp.sub(c_preproc.repl, code)
+			# add the language-specific output files as nodes
+			# call funs in the dict swig_langs
+			for x in self.env['SWIGFLAGS']:
+				# obtain the language
+				x = x[1:]
+				try:
+					fun = swig_langs[x]
+				except KeyError:
+					pass
+				else:
+					fun(self)
 
-		# find .i files and project headers
-		names = re_2.findall(code) + re_3.findall(code)
-		for n in names:
-			for d in self.generator.swig_dir_nodes + [node.parent]:
-				u = d.find_resource(n)
-				if u:
-					to_see.append(u)
-					break
-			else:
-				Logs.warn('could not find %r' % n)
+		return super(swig, self).runnable_status()
 
-	# list of nodes this one depends on, and module name if present
-	if Logs.verbose:
-		Logs.debug('deps: deps for %s: %s' % (str(self), str(lst_src)))
-	return (lst_src, [])
-cls.scan = scan
+	def scan(self):
+		"scan for swig dependencies, climb the .i files"
+		env = self.env
+
+		lst_src = []
+
+		seen = []
+		to_see = [self.inputs[0]]
+
+		while to_see:
+			node = to_see.pop(0)
+			if node.id in seen:
+				continue
+			seen.append(node.id)
+			lst_src.append(node)
+
+			# read the file
+			code = node.read()
+			code = c_preproc.re_nl.sub('', code)
+			code = c_preproc.re_cpp.sub(c_preproc.repl, code)
+
+			# find .i files and project headers
+			names = re_2.findall(code) + re_3.findall(code)
+			for n in names:
+				for d in self.generator.swig_dir_nodes + [node.parent]:
+					u = d.find_resource(n)
+					if u:
+						to_see.append(u)
+						break
+				else:
+					Logs.warn('could not find %r' % n)
+
+		return (lst_src, [])
 
 # provide additional language processing
 swig_langs = {}
@@ -143,7 +138,6 @@ def swig_ocaml(tsk):
 	tsk.set_outputs(tsk.inputs[0].parent.find_or_declare(tsk.module + '.ml'))
 	tsk.set_outputs(tsk.inputs[0].parent.find_or_declare(tsk.module + '.mli'))
 
-@taskgen
 @feature('swig')
 @after('apply_incpaths')
 def add_swig_paths(self):
@@ -171,12 +165,12 @@ def i_file(self, node):
 		flags.append(node.parent.abspath(self.env))
 
 @conf
-def check_swig_version(conf, minver=None):
+def check_swig_version(self, minver=None):
 	"""Check for a minimum swig version like conf.check_swig_version('1.3.28')
 	or conf.check_swig_version((1,3,28)) """
 	reg_swig = re.compile(r'SWIG Version\s(.*)', re.M)
 
-	swig_out = self.cmd_and_log('%s -version' % conf.env['SWIG'])
+	swig_out = self.cmd_and_log('%s -version' % self.env['SWIG'])
 
 	swigver = [int(s) for s in reg_swig.findall(swig_out)[0].split('.')]
 	if isinstance(minver, basestring):
@@ -186,14 +180,16 @@ def check_swig_version(conf, minver=None):
 	result = (minver is None) or (minver[:3] <= swigver[:3])
 	swigver_full = '.'.join(map(str, swigver))
 	if result:
-		conf.env['SWIG_VERSION'] = swigver_full
+		self.env['SWIG_VERSION'] = swigver_full
 	minver_str = '.'.join(map(str, minver))
 	if minver is None:
-		conf.check_message_custom('swig version', '', swigver_full)
+		msg = 'swig version'
 	else:
-		conf.check_message('swig version', '>= %s' % (minver_str,), result, option=swigver_full)
+		msg = 'swig version >= %s' % (minver_str,)
+
+	self.msg(msg, result)
 	return result
 
 def configure(conf):
-	swig = conf.find_program('swig', var='SWIG', mandatory=True)
+	swig = conf.find_program('swig', var='SWIG')
 
