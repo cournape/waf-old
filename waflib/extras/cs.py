cc2ef76e40ffcba90e0b9070133321fd90eafd23
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-06-22 07:32:53 +0000

    update the cs tool
    git-svn-id: http://waf.googlecode.com/svn/branches/waf-1.6@8511 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- waflib/extras/cs.py
+++ waflib/extras/cs.py
@@ -4,13 +4,16 @@
 
 """
 C# support
+
+We will need a demo to check that this works
+bld(features='cs', source='main.cs', target='foo')
 """
 
 from waflib import TaskGen, Utils, Task, Options, Logs
 from TaskGen import before, after, feature
 from waflib.Tools import ccroot
 
-ccroot.USELIB_VARS['cs'] = set(['FLAGS', 'ASSEMBLIES', 'RESOURCES'])
+ccroot.USELIB_VARS['cs'] = set(['CSFLAGS', 'ASSEMBLIES', 'RESOURCES'])
 
 @feature('cs')
 @after('apply_uselib_cs')
@@ -19,32 +22,24 @@ def apply_cs(self):
 	try: self.meths.remove('process_source')
 	except ValueError: pass
 
-	# process the flags for the assemblies
-	for i in self.to_list(self.assemblies) + self.env['ASSEMBLIES']:
-		self.env.append_unique('_ASSEMBLIES', '/r:'+i)
-
-	# process the flags for the resources
-	for i in self.to_list(self.resources):
-		self.env.append_unique('_RESOURCES', '/resource:'+i)
-
 	# what kind of assembly are we generating?
-	self.env['_TYPE'] = getattr(self, 'type', 'exe')
-
-	# additional flags
-	self.env.append_unique('_FLAGS', self.to_list(self.flags))
-	self.env.append_unique('_FLAGS', self.env.FLAGS)
+	self.env['CSTYPE'] = getattr(self, 'type', 'exe')
 
 	# process the sources
 	nodes = [self.path.find_resource(i) for i in self.to_list(self.source)]
 	self.create_task('mcs', nodes, self.path.find_or_declare(self.target))
 
-Task.task_factory('mcs', '${MCS} ${SRC} /target:${_TYPE} /out:${TGT} ${_FLAGS} ${_ASSEMBLIES} ${_RESOURCES}', color='YELLOW')
+class mcs(Task.Task):
+	color   = 'YELLOW'
+	run_str = '${MCS} ${SRC} /target:${CSTYPE} /out:${TGT} ${CSFLAGS} ${ASS_ST:ASSEMBLIES} ${RES_ST:RESOURCES}'
 
 def configure(conf):
 	csc = getattr(Options.options, 'cscbinary', None)
 	if csc:
 		conf.env.MCS = csc
 	conf.find_program(['gmcs', 'mcs'], var='MCS')
+	conf.env.ASS_ST = '/r:%s'
+	conf.env.RES_ST = '/resource:%s'
 
 def options(opt):
 	opt.add_option('--with-csc-binary', type='string', dest='cscbinary')
