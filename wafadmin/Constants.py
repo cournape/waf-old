894b4860e191f2318bcd74ebc9dc0a84e00c35e9
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-07-06 15:08:46 +0000

    docs
    git-svn-id: http://waf.googlecode.com/svn/trunk@8761 f0382ac9-c320-0410-b3f0-b508d59f5a85

--- wafadmin/Constants.py
+++ wafadmin/Constants.py
@@ -11,7 +11,7 @@ maintainer: the version number is updated from the top-level wscript file
 # do not touch these three lines, they are updated automatically
 HEXVERSION=0x10512
 WAFVERSION="1.5.18"
-WAFREVISION = "8005"
+WAFREVISION = "8760"
 ABI = 7
 
 # permissions
